e5d9c12fce7efade1ed64d4d52ac66e7617c844c
Matthijs Hollemans <mail@hollance.com>
2016-01-28 15:19:25 +0100

    Small tweaks to stack and queue playgrounds

--- Stack/Stack.playground/Contents.swift
+++ Stack/Stack.playground/Contents.swift
@@ -1,57 +1,60 @@
 /*
+  Stack
 
-Stack
+  A stack is like an array but with limited functionality. You can only push 
+  to add a new element to the top of the stack, pop to remove the element from
+  the top, and peek at the top element without popping it off.
 
-A stack is like an array but with limited functionality. You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off.
+  A stack gives you a LIFO or last-in first-out order. The element you pushed 
+  last is the first one to come off with the next pop.
 
-A stack gives you a LIFO or last-in first-out order. The element you pushed last is the first one to come off with the next pop. (A very similar data structure, the queue, is FIFO or first-in first-out.)
-
-Insertion - O(1)
-Deletion - O(1)
-
-
+  Push and pop are O(1) operations.
 */
 
-import UIKit
-
 public struct Stack<T> {
-    private var array = [T]()
-    
-    public var count: Int {
-        return array.count
+  private var array = [T]()
+  
+  public var count: Int {
+    return array.count
+  }
+  
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+  
+  public mutating func push(element: T) {
+    array.append(element)
+  }
+  
+  public mutating func pop() -> T? {
+    if count == 0 {
+      return nil
+    } else {
+      return array.removeLast()
     }
-    
-    public var isEmpty: Bool {
-        return array.isEmpty
-    }
-    
-    public mutating func push(element: T) {
-        array.append(element)
-    }
-    
-    public mutating func pop() -> T? {
-        if count == 0 {
-            return nil
-        } else {
-            return array.removeLast()
-        }
-    }
-    
-    public func peek() -> T? {
-        return array.last
-    }
-    
+  }
+  
+  public func peek() -> T? {
+    return array.last
+  }
 }
 
+// Create a stack and put some elements on it already.
+var stackOfNames = Stack(array: ["Carl", "Lisa", "Stephanie", "Jeff", "Wade"])
 
-var stackOfNames = Stack(array: ["Carl", "Lisa", "Stephanie", "Jeff", "Wade"]) // Initialized queue
+// Add an element to the top of the stack.
+stackOfNames.push("Mike")
 
-stackOfNames.push("Mike") // Adds element to the end of the stack. Stack is now ["Carl", "Lisa", "Stephanie", "Jeff", "Wade", "Mike"]
+// The stack is now ["Carl", "Lisa", "Stephanie", "Jeff", "Wade", "Mike"]
+print(stackOfNames.array)
 
-stackOfNames.pop() // Removes and returns the last element in the stack. Returns "Mike" and then removes it from the stack
+// Remove and return the first element from the stack. This returns "Mike".
+stackOfNames.pop()
 
-stackOfNames.peek() // Returns the first element in the stack. Returns "Wade" since "Mike" was popped on the previous line
+// Look at the first element from the stack.
+// Returns "Wade" since "Mike" was popped on the previous line.
+stackOfNames.peek()
 
-stackOfNames.isEmpty // Checks to see if the stack is empty. Returns "False" since the stack still has elements in it
-
-
+// Check to see if the stack is empty.
+// Returns "false" since the stack still has elements in it.
+stackOfNames.isEmpty
