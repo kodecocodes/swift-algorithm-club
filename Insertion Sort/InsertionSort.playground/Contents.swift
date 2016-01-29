e1198f86cf72e45df0a4c0b44cb609443e01796e
Matthijs Hollemans <mail@hollance.com>
2016-01-29 13:53:15 +0100

    Add playgrounds where missing

---
+++ Insertion Sort/InsertionSort.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+func insertionSort<T>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+  var a = array
+  for x in 1..<a.count {
+    var y = x
+    let temp = a[y]
+    while y > 0 && isOrderedBefore(temp, a[y - 1]) {
+      a[y] = a[y - 1]
+      y -= 1
+    }
+    a[y] = temp
+  }
+  return a
+}
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(list, <)
+insertionSort(list, >)
---
+++ Selection Sort/SelectionSort.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+func selectionSort<T>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+  guard array.count > 1 else { return array }
+  var a = array
+  for x in 0 ..< a.count - 1 {
+    var lowest = x
+    for y in x + 1 ..< a.count {
+      if isOrderedBefore(a[y], a[lowest]) {
+        lowest = y
+      }
+    }
+    if x != lowest {
+      swap(&a[x], &a[lowest])
+    }
+  }
+  return a
+}
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+selectionSort(list, <)
+selectionSort(list, >)
