42b12e8e5d34c5109abeabd3697c32f20d03abca
macbellingrath <macbellingrath@gmail.com>
2016-01-30 11:21:26 -0500

    Add singly linked list

---
+++ SinglyLinkedList/LinkedListTests/LinkedListTests/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LinkedListTests
+//
+//  Created by Mac Bellingrath on 1/30/16.
+//  Copyright © 2016 Mac Bellingrath. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
