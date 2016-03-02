3467142bca114217f8393bcf19c5d99b80d81b62
John Gill <king2bgill@gmail.com>
2016-03-01 22:17:51 -0800

    Apply type constraint Comparable

--- Bounded Priority Queue/BoundedPriorityQueue.swift
+++ Bounded Priority Queue/BoundedPriorityQueue.swift
@@ -1,18 +1,16 @@
 //
 //  BoundedPriorityQueue.swift
-//  Tests
 //
 //  Created by John Gill on 2/28/16.
-//  Copyright © 2016 Swift Algorithm Club. All rights reserved.
 //
 
 import Foundation
 
-public class LinkedListNode<T> {
+public class LinkedListNode<T: Comparable> {
     var value: T
     var next: LinkedListNode?
     var previous: LinkedListNode?
-    
+
     public init(value: T) {
         self.value = value
         self.next = nil
@@ -20,52 +18,50 @@ public class LinkedListNode<T> {
     }
 }
 
-public class BoundedPriorityQueue<T> {
+public class BoundedPriorityQueue<T: Comparable> {
     public typealias Node = LinkedListNode<T>
     private var head: Node?
-    private var isLessThan:(T, T) -> Bool
     private var curNumElements:Int
     private var maxNumElements: Int
-    
-    public init(sort: (T, T) -> Bool, maxElements:Int) {
-        isLessThan = sort
+
+    public init(maxElements:Int) {
         head = nil
         maxNumElements = maxElements
         curNumElements = 0
     }
-    
+
     public var isEmpty: Bool {
         return curNumElements == 0
     }
-    
+
     public var count: Int {
         return curNumElements
     }
-    
+
     public func peek() -> T? {
         if curNumElements > 0 {
             return head!.value
         }
         return nil
     }
-    
+
     public func enqueue(value: T) {
         let newNode = Node(value: value)
-        
+
         if head == nil  {
             head = newNode
         } else {
             var node = head
-            if curNumElements == maxNumElements && !isLessThan(newNode.value, node!.value) {
+            if curNumElements == maxNumElements && newNode.value > node!.value {
                 return
             }
 
-            while node!.next != nil && isLessThan(newNode.value, node!.value) { node = node!.next }
-            
-            if isLessThan(newNode.value, node!.value) {
+            while (node!.next != nil) && (newNode.value < node!.value) { node = node!.next }
+
+            if newNode.value < node!.value {
                 newNode.next = node!.next
                 newNode.previous = node
-                
+
                 if(newNode.next != nil) { /* TAIL */
                     newNode.next!.previous = newNode
                 }
@@ -82,13 +78,13 @@ public class BoundedPriorityQueue<T> {
             }
             if(curNumElements == maxNumElements) { dequeue() }
         }
-        curNumElements++
+        curNumElements += 1
     }
-    
+
     public func dequeue() -> T? {
         if curNumElements > 0 {
             let retVal = head!.value
-            
+
             if curNumElements == 1 {
                 head = nil
             }
@@ -97,7 +93,7 @@ public class BoundedPriorityQueue<T> {
                 head!.previous = nil
             }
 
-            curNumElements--
+            curNumElements -= 1
             return retVal
         }
         return nil
