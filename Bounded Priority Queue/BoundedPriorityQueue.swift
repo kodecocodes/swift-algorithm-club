2e27f222b1e327859753dc3363319bc95ffa97a6
Matthijs Hollemans <mail@hollance.com>
2016-03-03 19:46:58 +0100

    Clean up of Bounded Priority Queue

--- Bounded Priority Queue/BoundedPriorityQueue.swift
+++ Bounded Priority Queue/BoundedPriorityQueue.swift
@@ -4,98 +4,95 @@
 //  Created by John Gill on 2/28/16.
 //
 
-import Foundation
-
 public class LinkedListNode<T: Comparable> {
-    var value: T
-    var next: LinkedListNode?
-    var previous: LinkedListNode?
-
-    public init(value: T) {
-        self.value = value
-        self.next = nil
-        self.previous = nil
-    }
+  var value: T
+  var next: LinkedListNode?
+  var previous: LinkedListNode?
+  
+  public init(value: T) {
+    self.value = value
+    self.next = nil
+    self.previous = nil
+  }
 }
 
 public class BoundedPriorityQueue<T: Comparable> {
-    public typealias Node = LinkedListNode<T>
-    private var head: Node?
-    private var curNumElements:Int
-    private var maxNumElements: Int
+  private typealias Node = LinkedListNode<T>
 
-    public init(maxElements:Int) {
-        head = nil
-        maxNumElements = maxElements
-        curNumElements = 0
+  private(set) public var count = 0
+  private var head: Node?
+  private var maxElements: Int
+
+  public init(maxElements: Int) {
+    self.maxElements = maxElements
+  }
+
+  public var isEmpty: Bool {
+    return count == 0
+  }
+
+  public func peek() -> T? {
+    return head?.value
+  }
+
+  public func enqueue(value: T) {
+    let newNode = Node(value: value)
+
+    if head == nil  {
+      head = newNode
+      count = 1
+      return
     }
 
-    public var isEmpty: Bool {
-        return curNumElements == 0
+    var node = head
+    if count == maxElements && newNode.value > node!.value {
+      return
+    }
+    
+    while (node!.next != nil) && (newNode.value < node!.value) {
+      node = node!.next
+    }
+    
+    if newNode.value < node!.value {
+      newNode.next = node!.next
+      newNode.previous = node
+      
+      if newNode.next != nil {     /* TAIL */
+        newNode.next!.previous = newNode
+      }
+      node!.next = newNode
+    } else {
+      newNode.previous = node!.previous
+      newNode.next = node
+      if node!.previous == nil {   /* HEAD */
+        head = newNode
+      } else {
+        node!.previous!.next = newNode
+      }
+      node!.previous = newNode
     }
 
-    public var count: Int {
-        return curNumElements
+    if count == maxElements {
+      dequeue()
+    }
+    count += 1
+  }
+  
+  public func dequeue() -> T? {
+    if count == 0 {
+      return nil
     }
 
-    public func peek() -> T? {
-        if curNumElements > 0 {
-            return head!.value
-        }
-        return nil
+    let retVal = head!.value
+    
+    if count == 1 {
+      head = nil
+    } else {
+      head = head!.next
+      head!.previous = nil
     }
 
-    public func enqueue(value: T) {
-        let newNode = Node(value: value)
-
-        if head == nil  {
-            head = newNode
-        } else {
-            var node = head
-            if curNumElements == maxNumElements && newNode.value > node!.value {
-                return
-            }
-
-            while (node!.next != nil) && (newNode.value < node!.value) { node = node!.next }
-
-            if newNode.value < node!.value {
-                newNode.next = node!.next
-                newNode.previous = node
-
-                if(newNode.next != nil) { /* TAIL */
-                    newNode.next!.previous = newNode
-                }
-                node!.next = newNode
-            } else {
-                newNode.previous = node!.previous
-                newNode.next = node
-                if(node!.previous == nil) { /* HEAD */
-                    head = newNode
-                } else {
-                    node!.previous!.next = newNode
-                }
-                node!.previous = newNode
-            }
-            if(curNumElements == maxNumElements) { dequeue() }
-        }
-        curNumElements += 1
-    }
-
-    public func dequeue() -> T? {
-        if curNumElements > 0 {
-            let retVal = head!.value
-
-            if curNumElements == 1 {
-                head = nil
-            }
-            else {
-                head = head!.next
-                head!.previous = nil
-            }
-
-            curNumElements -= 1
-            return retVal
-        }
-        return nil
-    }
+    count -= 1
+    return retVal
+  }
 }
