78af3a3ab7e70c1347d5044bca45d9794e5d38d0
Matthijs Hollemans <mail@hollance.com>
2016-03-05 10:10:56 +0100

    Rename variable to matrix

--- Array2D/Array2D.playground/Contents.swift
+++ Array2D/Array2D.playground/Contents.swift
@@ -1,65 +1,64 @@
 /*
-Two-dimensional array with a fixed number of rows and columns.
-This is mostly handy for games that are played on a grid, such as chess.
-Performance is always O(1).
+  Two-dimensional array with a fixed number of rows and columns.
+  This is mostly handy for games that are played on a grid, such as chess.
+  Performance is always O(1).
 */
 public struct Array2D<T> {
-    public let columns: Int
-    public let rows: Int
-    private var array: [T]
-    
-    public init(columns: Int, rows: Int, initialValue: T) {
-        self.columns = columns
-        self.rows = rows
-        array = .init(count: rows*columns, repeatedValue: initialValue)
+  public let columns: Int
+  public let rows: Int
+  private var array: [T]
+  
+  public init(columns: Int, rows: Int, initialValue: T) {
+    self.columns = columns
+    self.rows = rows
+    array = .init(count: rows*columns, repeatedValue: initialValue)
+  }
+  
+  public subscript(column: Int, row: Int)  -> T {
+    get {
+      return array[row*columns + column]
     }
-    
-    public subscript(column: Int, row: Int)  -> T {
-        get {
-            return array[row*columns + column]
-        }
-        set {
-            array[row*columns + column] = newValue
-        }
+    set {
+      array[row*columns + column] = newValue
     }
-
+  }
 }
+
+
 
 // initialization
-var Array2DNumbers = Array2D(columns: 3, rows: 5, initialValue: 0)
+var matrix = Array2D(columns: 3, rows: 5, initialValue: 0)
 
 // makes an array of rows * columns elements all filled with zero
-print(Array2DNumbers.array)
+print(matrix.array)
 
 // setting numbers using subscript [x, y]
-Array2DNumbers[0, 0] = 1
-Array2DNumbers[1, 0] = 2
+matrix[0, 0] = 1
+matrix[1, 0] = 2
 
-Array2DNumbers[0, 1] = 3
-Array2DNumbers[1, 1] = 4
+matrix[0, 1] = 3
+matrix[1, 1] = 4
 
-Array2DNumbers[0, 2] = 5
-Array2DNumbers[1, 2] = 6
+matrix[0, 2] = 5
+matrix[1, 2] = 6
 
-Array2DNumbers[0, 3] = 7
-Array2DNumbers[1, 3] = 8
-Array2DNumbers[2, 3] = 9
+matrix[0, 3] = 7
+matrix[1, 3] = 8
+matrix[2, 3] = 9
 
 // now the numbers are set in the array
-print(Array2DNumbers.array)
+print(matrix.array)
 
 // print out the 2D array with a reference around the grid
-for i in 0..<Array2DNumbers.rows {
-    print("[", terminator: "");
-    for j in 0..<Array2DNumbers.columns {
-        if (j == Array2DNumbers.columns - 1) {
-            print("\(Array2DNumbers[j, i])", terminator: "");
-        } else {
-            print("\(Array2DNumbers[j, i]) ", terminator: "");
-        }
+for i in 0..<matrix.rows {
+  print("[", terminator: "");
+  for j in 0..<matrix.columns {
+    if (j == matrix.columns - 1) {
+      print("\(matrix[j, i])", terminator: "");
+    } else {
+      print("\(matrix[j, i]) ", terminator: "");
     }
-    print("]");
+  }
+  print("]");
 }
-
-
 
