da7c065fa62a17bfb97bec9a19de259aa647d12f
Siddharth Atre <sidatre@gmail.com>
2016-03-15 14:47:10 -0700

    Created TST and TSTNode class declarations.

---
+++ TernarySearchTree/TSTNode.swift
@@ -0,0 +1,13 @@
+//
+//  TSTNode.swift
+//  
+//
+//  Created by Siddharth Atre on 3/15/16.
+//
+//
+
+import Foundation
+
+class TSTNode {
+    
+}
---
+++ TernarySearchTree/TernarySearchTree.swift
@@ -0,0 +1,13 @@
+//
+//  TernarySearchTree.swift
+//  
+//
+//  Created by Siddharth Atre on 3/15/16.
+//
+//
+
+import Foundation
+
+class TernarySearchTree {
+    
+}
