ef914bee2bd3b6d25888afb73604332f6c2a91b7
Siddharth Atre <sidatre@gmail.com>
2016-03-17 11:58:14 -0700

    Made initializers for TSTNode

--- Ternary Search Tree/TSTNode.swift
+++ Ternary Search Tree/TSTNode.swift
@@ -8,6 +8,21 @@
 
 import Foundation
 
-class TSTNode {
+class TSTNode<Element> {
+    var key: String
+    var data: Element?
+    var isKey: Bool
+    
+    init(key: String, data: Element?) {
+        self.key = key
+        self.data = data
+        self.isKey = (data != nil)
+    }
+    
+    init(key:String) {
+        self.key = key
+        self.data = nil
+        self.isKey = false
+    }
     
 }
