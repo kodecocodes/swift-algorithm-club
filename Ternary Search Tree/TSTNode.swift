79113d99e4187641c9715d3b91911fa4fccb7b5a
Siddharth Atre <sidatre@gmail.com>
2016-03-17 12:08:26 -0700

    Changed key variable to be of type Character

--- Ternary Search Tree/TSTNode.swift
+++ Ternary Search Tree/TSTNode.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class TSTNode<Element> {
     //Member properties of a particular node.
-    var key: String
+    var key: Character
     var data: Element?
     var isKey: Bool
     
@@ -18,13 +18,13 @@ class TSTNode<Element> {
     var left: TSTNode?, right: TSTNode?, middle: TSTNode?
     
     
-    init(key: String, data: Element?) {
+    init(key: Character, data: Element?) {
         self.key = key
         self.data = data
         self.isKey = (data != nil)
     }
     
-    init(key:String) {
+    init(key:Character) {
         self.key = key
         self.data = nil
         self.isKey = false
