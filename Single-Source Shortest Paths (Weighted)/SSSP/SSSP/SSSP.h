98dcf99f8dcf1dd691334ce8ce3e26e604ce86bc
Andrew McKnight <amcknight2718@gmail.com>
2016-05-08 21:26:59 -0400

    create SSSP project

---
+++ Single-Source Shortest Paths (Weighted)/SSSP/SSSP/SSSP.h
@@ -0,0 +1,19 @@
+//
+//  SSSP.h
+//  SSSP
+//
+//  Created by Andrew McKnight on 5/8/16.
+//  Copyright © 2016 Andrew McKnight. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SSSP.
+FOUNDATION_EXPORT double SSSPVersionNumber;
+
+//! Project version string for SSSP.
+FOUNDATION_EXPORT const unsigned char SSSPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSSP/PublicHeader.h>
+
+
