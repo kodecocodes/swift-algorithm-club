dd3ba8df3f9a24d521192af18a61a07d39134451
Andrew McKnight <amcknight2718@gmail.com>
2016-05-30 13:24:00 -0400

    add project for SSSP with Bellman-Ford implementation

---
+++ Single-Source Shortest Paths (Weighted)/SSSP/SSSP.h
@@ -0,0 +1,18 @@
+//
+//  SSSP.h
+//  SSSP
+//
+//  Created by Andrew McKnight on 5/8/16.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SSSP.
+FOUNDATION_EXPORT double SSSPVersionNumber;
+
+//! Project version string for SSSP.
+FOUNDATION_EXPORT const unsigned char SSSPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSSP/PublicHeader.h>
+
+
