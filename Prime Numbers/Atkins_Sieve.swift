fbba3db3ac1edacdf90e177e8a96cfda2981a886
Pratikbhai Patel <pratikpatel@me.com>
2016-06-13 10:18:07 -0400

    Add Atkins’ sieve Rename files Add project

--- Prime Numbers/Atkins_Sieve.swift
+++ Prime Numbers/Atkins_Sieve.swift
@@ -1,8 +1,3 @@
-//: Playground - noun: a place where people can play
-
-import Foundation
-import UIKit
-
 infix operator ^^ { associativity left precedence 160 }
 func ^^ (radix: Int, power: Int) -> Int {
     return Int(pow(CGFloat(radix), CGFloat(power)))
@@ -18,32 +13,8 @@ func ..<T: Strideable>(left: (T, T.Stride), right: T) -> [T] {
     return [T](left.0.stride(through: right, by: left.1))
 }
 
-func eratosthenes_sieve(max: Int) -> [Int] {
-    assert(max > 1, "Prime numbers can only be above 1")
-    print("Getting all primes under \(max)")
-    let m = Int(sqrt(ceil(Double(max))))
-    print("Need to remove all multiples of numbers through \(m)")
-    let set = NSMutableSet(array: 3..2..max)
-    set.addObject(2)
-    print(set)
-    for i in (2..1..m) {
-        if (set.containsObject(i)) {
-            print("Removing all multiples of \(i)")
-            for j in i^^2..i..max {
-                print("removing j: \(j)")
-                set.removeObject(j)
-            }
-        }
-    }
-    return set.sortedArrayUsingDescriptors([NSSortDescriptor(key: "integerValue", ascending: true)]) as! [Int]
-}
 
-var startDate = NSDate().timeIntervalSince1970 * 1000
-print(eratosthenes_sieve(5000))
-var endDate = NSDate().timeIntervalSince1970 * 1000
-print("Prime generation time: \(endDate - startDate) ms.")
-
-func atkins_sieve(max: Int) -> [Int] {
+func primesTo(max: Int) -> [Int] {
     var is_prime = [Bool](count: max + 1, repeatedValue: false)
     is_prime[2] = true
     is_prime[3] = true
@@ -81,8 +52,3 @@ func atkins_sieve(max: Int) -> [Int] {
     }
     return primesArray
 }
-
-startDate = NSDate().timeIntervalSince1970 * 1000
-print(atkins_sieve(5000))
-endDate = NSDate().timeIntervalSince1970 * 1000
-print("Prime generation time: \(endDate - startDate) ms.")
---
+++ Prime Numbers/Primes/Primes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Primes
+//
+//  Created by Pratikbhai Patel on 6/13/16.
+//  Copyright © 2016 Pratikbhai Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
