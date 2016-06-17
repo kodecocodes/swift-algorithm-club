7dd0cb5cca1baa5115b5e111454d279cef42ad03
Viktor Szilárd Viktor <aqviktor@gmail.com>
2016-06-17 13:59:36 +0200

    Added playground contents.

--- B-Tree/BTree.playground/Contents.swift
+++ B-Tree/BTree.playground/Contents.swift
@@ -8,3 +8,19 @@ bTree.insertValue(1, forKey: 1)
 bTree.insertValue(2, forKey: 2)
 bTree.insertValue(3, forKey: 3)
 bTree.insertValue(4, forKey: 4)
+
+bTree.valueForKey(3)
+bTree[3]
+
+bTree.removeKey(2)
+
+bTree.traverseKeysInOrder {
+  key in
+  print(key)
+}
+
+bTree.numberOfKeys
+
+bTree.order
+
+bTree.inorderArrayFromKeys()
