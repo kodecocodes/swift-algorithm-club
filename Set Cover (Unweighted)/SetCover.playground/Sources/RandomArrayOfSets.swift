1cfb7de3ff686532b816469f9251272b7c7fec15
Chris Pilcher <nzchrisp@gmail.com>
2016-06-28 16:09:53 +0200

    Removing swiftlint warnings from Set Cover (Unweighted)

--- Set Cover (Unweighted)/SetCover.playground/Sources/RandomArrayOfSets.swift
+++ Set Cover (Unweighted)/SetCover.playground/Sources/RandomArrayOfSets.swift
@@ -5,17 +5,17 @@ public func randomArrayOfSets<T>(covering universe: Set<T>,
                               maxSetSizeFactor: Double = 0.6) -> Array<Set<T>> {
   var result = [Set<T>]()
   var ongoingUnion = Set<T>()
-  
+
   let minArraySize = Int(Double(universe.count) * minArraySizeFactor)
   var maxSetSize = Int(Double(universe.count) * maxSetSizeFactor)
   if maxSetSize > universe.count {
     maxSetSize = universe.count
   }
-  
+
   while true {
     var generatedSet = Set<T>()
     let targetSetSize = Int(arc4random_uniform(UInt32(maxSetSize)) + 1)
-    
+
     while true {
       let randomUniverseIndex = Int(arc4random_uniform(UInt32(universe.count)))
       for (setIndex, value) in universe.enumerate() {
@@ -24,20 +24,20 @@ public func randomArrayOfSets<T>(covering universe: Set<T>,
           break
         }
       }
-      
+
       if generatedSet.count == targetSetSize {
         result.append(generatedSet)
         ongoingUnion = ongoingUnion.union(generatedSet)
         break
       }
     }
-    
+
     if result.count >= minArraySize {
       if ongoingUnion == universe {
         break
       }
     }
   }
-  
+
   return result
 }
--- Set Cover (Unweighted)/SetCover.swift
+++ Set Cover (Unweighted)/SetCover.swift
@@ -2,11 +2,11 @@ extension Set {
   func cover(within array: Array<Set<Element>>) -> Array<Set<Element>>? {
     var result: [Set<Element>]? = [Set<Element>]()
     var remainingSet = self
-    
+
     func largestIntersectingSet() -> Set<Element>? {
       var largestIntersectionLength = 0
       var largestSet: Set<Element>?
-      
+
       for set in array {
         let intersectionLength = remainingSet.intersect(set).count
         if intersectionLength > largestIntersectionLength {
@@ -14,20 +14,20 @@ extension Set {
           largestSet = set
         }
       }
-      
+
       return largestSet
     }
-    
+
     while remainingSet.count != 0 {
       guard let largestSet = largestIntersectingSet() else { break }
       result!.append(largestSet)
       remainingSet = remainingSet.subtract(largestSet)
     }
-    
+
     if remainingSet.count != 0 || self.count == 0 {
       result = nil
     }
-    
+
     return result
   }
 }
