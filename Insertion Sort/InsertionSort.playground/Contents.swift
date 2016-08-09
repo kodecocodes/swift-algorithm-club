8cf7538a76f416508ab3c84215cd9697fbeb42e2
velyan <veli@MAC-IT-VY.local>
2016-08-09 15:55:08 +0300

    Refactored copy-in copy-out by using inout keyword to make the code more concise.

--- Insertion Sort/InsertionSort.playground/Contents.swift
+++ Insertion Sort/InsertionSort.playground/Contents.swift
@@ -1,19 +1,19 @@
 //: Playground - noun: a place where people can play
 
-func insertionSort<T>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
-  var a = array
-  for x in 1..<a.count {
-    var y = x
-    let temp = a[y]
-    while y > 0 && isOrderedBefore(temp, a[y - 1]) {
-      a[y] = a[y - 1]
-      y -= 1
+func insertionSort<T>(inout array: [T], _ isOrderedBefore: (T, T) -> Bool) {
+    guard array.count > 1 else { return }
+    
+    for x in 1..<array.count {
+        var y = x
+        let temp = array[y]
+        while y > 0 && isOrderedBefore(temp, array[y - 1]) {
+            array[y] = array[y - 1]
+            y -= 1
+        }
+        array[y] = temp
     }
-    a[y] = temp
-  }
-  return a
 }
 
-let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
-insertionSort(list, <)
-insertionSort(list, >)
+var list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(&list, <)
+insertionSort(&list, >)
--- Insertion Sort/InsertionSort.swift
+++ Insertion Sort/InsertionSort.swift
@@ -1,15 +1,13 @@
-func insertionSort<T>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
-  guard array.count > 1 else { return array }
+func insertionSort<T>(inout array: [T], _ isOrderedBefore: (T, T) -> Bool) {
+  guard array.count > 1 else { return }
 
-  var a = array
-  for x in 1..<a.count {
+  for x in 1..<array.count {
     var y = x
-    let temp = a[y]
-    while y > 0 && isOrderedBefore(temp, a[y - 1]) {
-      a[y] = a[y - 1]
+    let temp = array[y]
+    while y > 0 && isOrderedBefore(temp, array[y - 1]) {
+      array[y] = array[y - 1]
       y -= 1
     }
-    a[y] = temp
+    array[y] = temp
   }
-  return a
 }
