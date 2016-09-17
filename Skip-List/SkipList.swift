8aeacb054d85a3024675fb0985e369a1e01add86
Mike Taghavi <mitghi@me.com>
2016-09-17 23:55:39 +0200

    New style / Broken down into extensions

--- Skip-List/SkipList.swift
+++ Skip-List/SkipList.swift
@@ -26,241 +26,268 @@ import Foundation
 
 // Stack from : https://github.com/raywenderlich/swift-algorithm-club/tree/master/Stack
 public struct Stack<T> {
-    private var array = [T]()
+  fileprivate var array = [T]()
 
-    public var isEmpty: Bool {
-        return array.isEmpty
-    }
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
 
-    public var count: Int {
-        return array.count
-    }
+  public var count: Int {
+    return array.count
+  }
 
-    public mutating func push(element: T) {
-        array.append(element)
-    }
+  public mutating func push(_ element: T) {
+    array.append(element)
+  }
 
-    public mutating func pop() -> T? {
-        return array.popLast()
-    }
+  public mutating func pop() -> T? {
+    return array.popLast()
+  }
 
-    public func peek() -> T? {
-        return array.last
-    }
+  public func peek() -> T? {
+    return array.last
+  }
 }
 
-extension Stack: SequenceType {
-    public func generate() -> AnyGenerator<T> {
-        var curr = self
-        return AnyGenerator {
-                   _ -> T? in
-                   return curr.pop()
-            }
-    }
-}
-
-
-func random() -> Bool {
-    #if os(Linux)
-        return random() % 2 == 0
-    #elseif os(OSX)
-        return arc4random_uniform(2) == 1
-    #endif
+extension Stack: Sequence {
+  public func makeIterator() -> AnyIterator<T> {
+    var curr = self
+    return AnyIterator {
+             _ -> T? in
+             return curr.pop()
+      }
+  }
 }
 
 
-
-class DataNode<Key: Comparable, Payload>{
-    internal typealias Node = DataNode<Key,Payload>
-    
-    internal var key   : Key?
-    internal var data  : Payload?
-    internal var _next : Node?
-    internal var _down : Node?
-
-    internal var next: Node? {
-        get        { return self._next  }
-        set(value) { self._next = value }        
-    }
-
-    internal var down:Node? {
-        get        { return self._down  }
-        set(value) { self._down = value }
-    }
-    
-    init(key:Key, data:Payload){
-        self.key  = key
-        self.data = data
-    }
-
-    init(header: Bool){        
-    }
-    
+private func coinFlip() -> Bool {
+  #if os(Linux)
+   return random() % 2 == 0
+  #elseif os(OSX)
+   return arc4random_uniform(2) == 1
+  #endif
 }
 
 
-class SkipList<Key: Comparable, Payload>{
-    internal typealias Node = DataNode<Key, Payload>
-    internal var head: Node?
+// MARK: - Node
 
-    private func find_head(key:Key) -> Node? {
-        var current     = self.head
-        var found: Bool = false
+public class DataNode<Key: Comparable, Payload> {
+  public typealias Node = DataNode<Key, Payload>
+  
+  var data            : Payload?  
+  fileprivate var key : Key?
+  internal var next   : Node?
+  internal var down   : Node?
+  
+  public init(key: Key, data: Payload) {
+    self.key  = key
+    self.data = data
+  }
 
-        while !found {
-            if let curr = current {
-                if curr.next == nil { current = curr.down }
-                else {
-                    if curr.next!.key == key { found = true }
-                    else {                        
-                        if key < curr.next!.key{ current = curr.down }
-                        else { current = curr.next }
-                    }
-                }
+  public init(asHead head: Bool){
+  }
+  
+}
+
+
+// MARK: - Skip List
+
+open class SkipList<Key: Comparable, Payload> {
+  public typealias Node = DataNode<Key, Payload>
+  
+  fileprivate(set) var head: Node?
+
+  public init() { }
+  
+}
+
+
+
+// MARK: - Searching
+
+extension SkipList {
+  
+  internal func findNode(key: Key) -> Node? {
+    var currentNode : Node? = self.head
+    var isFound     : Bool  = false
+
+    while !isFound {
+      if let node = currentNode {
+        
+        switch node.next {
+        case .none:
+          
+          currentNode = node.down
+        case .some(let value) where value.key != nil:
+
+          if value.key == key {
+            isFound = true
+            break
+          }
+          else {
+            if key < value.key! {
+              currentNode = node.down
             } else {
-                break
-            }
+              currentNode = node.next
+            }            
+          }
+          
+        default:
+          continue
         }
+        
+      } else { 
+        break
+      }
+    }
 
-        if found {
-            return current
+    if isFound {
+      return currentNode
+    } else {
+      return nil
+    }
+    
+  }
+
+  internal func search(key: Key) -> Payload? {
+    guard let node = self.findNode(key: key) else {
+      return nil
+    }
+
+    return node.next!.data    
+  }    
+ 
+}
+
+
+
+// MARK: - Inserting
+
+extension SkipList {
+  private func bootstrapBaseLayer(key: Key, data: Payload) -> Void {    
+    self.head          = Node(asHead: true)    
+    var node           = Node(key: key, data: data)
+
+    self.head!.next    = node
+    
+    var currentTopNode = node
+
+    while coinFlip() {
+      let newHead    = Node(asHead: true)
+      node           = Node(key: key, data: data)
+      node.down      = currentTopNode
+      newHead.next   = node
+      newHead.down   = self.head
+      self.head      = newHead
+      currentTopNode = node
+    }
+    
+  }
+
+
+  private func insertItem(key: Key, data: Payload) -> Void {
+    var stack              = Stack<Node>()
+    var currentNode: Node? = self.head
+
+    while currentNode != nil {
+
+      if let nextNode = currentNode!.next {
+        if nextNode.key! > key {
+          stack.push(currentNode!)
+          currentNode = currentNode!.down
         } else {
-            return nil
-        }        
-    }
-    
-
-    private func insert_head(key:Key, data:Payload) -> Void {
-        
-        self.head       = Node(header: true)
-        var temp        = Node(key:key, data:data)
-        self.head!.next = temp
-        var top         = temp
-
-        while random() {
-            let newhead  = Node(header: true)
-            temp         = Node(key:key, data:data)
-            temp.down    = top
-            newhead.next = temp
-            newhead.down = self.head
-            self.head    = newhead
-            top          = temp                
-        }
-    }
-
-    private func insert_rest(key:Key, data:Payload) -> Void {
-        var stack = Stack<Node>()
-        var current_head: Node? = self.head
-
-        while current_head != nil {
-
-            if let next = current_head!._next {
-                if next.key > key {
-                    stack.push(current_head!)
-                    current_head = next
-                } else {
-                    current_head = next
-                }
-                
-            } else {
-                stack.push(current_head!)
-                current_head = current_head!.down                    
-            }
-            
-        }            
-
-        let lowest   = stack.pop()
-        var temp     = Node(key:key, data:data)
-        temp.next    = lowest!.next
-        lowest!.next = temp
-        var top      = temp
-
-        while random() {
-            if stack.isEmpty {
-                let newhead  = Node(header: true)
-                temp         = Node(key:key, data:data)
-                temp.down    = top
-                newhead.next = temp
-                newhead.down = self.head
-                self.head    = newhead
-                top          = temp
-            } else {
-                let next   = stack.pop()
-                temp       = Node(key:key, data:data)
-                temp.down  = top
-                temp.next  = next!.next
-                next!.next = temp
-                top        = temp
-            }
-        }        
-    }
-    
-    func search(key:Key) -> Payload? {
-        guard let item = self.find_head(key) else {
-            return nil
-        }
-
-        return item.next!.data        
-    }
-    
-    func remove(key:Key) -> Void {
-        guard let item = self.find_head(key) else {
-            return
+          currentNode = nextNode
         }
         
-        var curr = Optional(item)
-        
-        while curr != nil {
-            let node      = curr!.next
-            
-            if node!.key != key { curr = node ; continue }
+      } else {
+        stack.push(currentNode!)
+        currentNode = currentNode!.down           
+      }
+      
+    }       
 
-            let node_next = node!.next            
-            curr!.next    = node_next
-            curr          = curr!.down
-            
-        }
-        
-    }
-    
-    func insert(key: Key, data:Payload){
-        if self.head != nil{
-            if let node = self.find_head(key) {
-                
-                var curr = node.next                
-                while curr != nil && curr!.key == key{
-                    curr!.data = data
-                    curr       = curr!.down
-                }
-                
-            } else {                        
-                self.insert_rest(key, data:data)
-            }
-            
-        } else {
-            self.insert_head(key, data:data)
-        }
-    }
+    let itemAtLayer    = stack.pop()
+    var node           = Node(key: key, data: data)
+    node.next          = itemAtLayer!.next
+    itemAtLayer!.next  = node
+    var currentTopNode = node
 
+    while coinFlip() {
+      if stack.isEmpty {
+        let newHead    = Node(asHead: true)
+        
+        node           = Node(key: key, data: data)
+        node.down      = currentTopNode
+        newHead.next   = node
+        newHead.down   = self.head
+        self.head      = newHead
+        currentTopNode = node
+        
+      } else {        
+        let nextNode  = stack.pop()
+        
+        node           = Node(key: key, data: data)
+        node.down      = currentTopNode
+        node.next      = nextNode!.next
+        nextNode!.next = node
+        currentTopNode = node
+      }
+    }     
+  }
+
+
+  internal func insert(key: Key, data: Payload) {
+    if self.head != nil {
+      if let node = self.findNode(key: key) {
+        // replace in case key already exists
+        var currentNode = node.next        
+        while currentNode != nil && currentNode!.key == key {
+          currentNode!.data = data
+          currentNode       = currentNode!.down
+        }
+      } else {
+        self.insertItem(key: key, data: data)
+      }
+      
+    } else {
+      self.bootstrapBaseLayer(key: key, data: data)
+    }
+  }
+  
 }
 
 
-class Map<Key: Comparable, Payload>{    
-    var collection: SkipList<Key, Payload>
+// MARK: - Removing
 
-    init(){
-        self.collection = SkipList<Key, Payload>()
+extension SkipList {
+  public func remove(key: Key) -> Void {
+    guard let item = self.findNode(key: key) else {
+      return
     }
     
-    func insert(key:Key, data: Payload){
-        self.collection.insert(key, data:data)
-    }
+    var currentNode = Optional(item)
+    
+    while currentNode != nil {
+      let node   = currentNode!.next
+      
+      if node!.key != key {
+        currentNode = node
+        continue
+      }
 
-    func get(key:Key) -> Payload?{
-        return self.collection.search(key)
+      let nextNode      = node!.next
+      
+      currentNode!.next = nextNode
+      currentNode       = currentNode!.down
+      
     }
     
-    func remove(key:Key) -> Void {
-        return self.collection.remove(key)
-    }
+  }  
+}
+
+extension SkipList {
+  
+  public func get(key:Key) -> Payload?{
+    return self.search(key: key)
+  }
 }
