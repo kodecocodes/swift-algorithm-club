ee38a5a6055c12f823f3e2fc2852a299e8a04357
Vladimir Zamyatin <boba@opendsp.com>
2016-11-02 15:23:31 +0100

    Fix a bug in SegmentTree

--- Segment Tree/SegmentTree.playground/Contents.swift
+++ Segment Tree/SegmentTree.playground/Contents.swift
@@ -28,7 +28,7 @@ public class SegmentTree<T> {
 		self.init(array: array, leftBound: 0, rightBound: array.count-1, function: function)
 	}
 	
-	public func query(withLeftBound: Int, rightBound: Int) -> T {
+	public func query(leftBound: Int, rightBound: Int) -> T {
 		if self.leftBound == leftBound && self.rightBound == rightBound {
 			return self.value
 		}
@@ -37,12 +37,12 @@ public class SegmentTree<T> {
 		guard let rightChild = rightChild else { fatalError("rightChild should not be nil") }
 		
 		if leftChild.rightBound < leftBound {
-			return rightChild.query(withLeftBound: leftBound, rightBound: rightBound)
+			return rightChild.query(leftBound: leftBound, rightBound: rightBound)
 		} else if rightChild.leftBound > rightBound {
-			return leftChild.query(withLeftBound: leftBound, rightBound: rightBound)
+			return leftChild.query(leftBound: leftBound, rightBound: rightBound)
 		} else {
-			let leftResult = leftChild.query(withLeftBound: leftBound, rightBound: leftChild.rightBound)
-			let rightResult = rightChild.query(withLeftBound:rightChild.leftBound, rightBound: rightBound)
+			let leftResult = leftChild.query(leftBound: leftBound, rightBound: leftChild.rightBound)
+			let rightResult = rightChild.query(leftBound:rightChild.leftBound, rightBound: rightBound)
 			return function(leftResult, rightResult)
 		}
 	}
@@ -68,14 +68,14 @@ let array = [1, 2, 3, 4]
 
 let sumSegmentTree = SegmentTree(array: array, function: +)
 
-print(sumSegmentTree.query(withLeftBound: 0, rightBound: 3)) // 1 + 2 + 3 + 4 = 10
-print(sumSegmentTree.query(withLeftBound: 1, rightBound: 2)) // 2 + 3 = 5
-print(sumSegmentTree.query(withLeftBound: 0, rightBound: 0)) // 1 = 1
+print(sumSegmentTree.query(leftBound: 0, rightBound: 3)) // 1 + 2 + 3 + 4 = 10
+print(sumSegmentTree.query(leftBound: 1, rightBound: 2)) // 2 + 3 = 5
+print(sumSegmentTree.query(leftBound: 0, rightBound: 0)) // 1 = 1
 
 sumSegmentTree.replaceItem(at: 0, withItem: 2) //our array now is [2, 2, 3, 4]
 
-print(sumSegmentTree.query(withLeftBound: 0, rightBound: 0)) // 2 = 2
-print(sumSegmentTree.query(withLeftBound: 0, rightBound: 1)) // 2 + 2 = 4
+print(sumSegmentTree.query(leftBound: 0, rightBound: 0)) // 2 = 2
+print(sumSegmentTree.query(leftBound: 0, rightBound: 1)) // 2 + 2 = 4
 
 
 //you can use any associative function (i.e (a+b)+c == a+(b+c)) as function for segment tree
@@ -96,14 +96,14 @@ let gcdArray = [2, 4, 6, 3, 5]
 
 let gcdSegmentTree = SegmentTree(array: gcdArray, function: gcd)
 
-print(gcdSegmentTree.query(withLeftBound: 0, rightBound: 1)) // gcd(2, 4) = 2
-print(gcdSegmentTree.query(withLeftBound: 2, rightBound: 3)) // gcd(6, 3) = 3
-print(gcdSegmentTree.query(withLeftBound: 1, rightBound: 3)) // gcd(4, 6, 3) = 1
-print(gcdSegmentTree.query(withLeftBound: 0, rightBound: 4)) // gcd(2, 4, 6, 3, 5) = 1
+print(gcdSegmentTree.query(leftBound: 0, rightBound: 1)) // gcd(2, 4) = 2
+print(gcdSegmentTree.query(leftBound: 2, rightBound: 3)) // gcd(6, 3) = 3
+print(gcdSegmentTree.query(leftBound: 1, rightBound: 3)) // gcd(4, 6, 3) = 1
+print(gcdSegmentTree.query(leftBound: 0, rightBound: 4)) // gcd(2, 4, 6, 3, 5) = 1
 
 gcdSegmentTree.replaceItem(at: 3, withItem: 10) //gcdArray now is [2, 4, 6, 10, 5]
 
-print(gcdSegmentTree.query(withLeftBound: 3, rightBound: 4)) // gcd(10, 5) = 5
+print(gcdSegmentTree.query(leftBound: 3, rightBound: 4)) // gcd(10, 5) = 5
 
 
 //example of segment tree which finds minimum on given range
@@ -111,12 +111,12 @@ let minArray = [2, 4, 1, 5, 3]
 
 let minSegmentTree = SegmentTree(array: minArray, function: min)
 
-print(minSegmentTree.query(withLeftBound: 0, rightBound: 4)) // min(2, 4, 1, 5, 3) = 1
-print(minSegmentTree.query(withLeftBound: 0, rightBound: 1)) // min(2, 4) = 2
+print(minSegmentTree.query(leftBound: 0, rightBound: 4)) // min(2, 4, 1, 5, 3) = 1
+print(minSegmentTree.query(leftBound: 0, rightBound: 1)) // min(2, 4) = 2
 
 minSegmentTree.replaceItem(at: 2, withItem: 10) // minArray now is [2, 4, 10, 5, 3]
 
-print(minSegmentTree.query(withLeftBound: 0, rightBound: 4)) // min(2, 4, 10, 5, 3) = 2
+print(minSegmentTree.query(leftBound: 0, rightBound: 4)) // min(2, 4, 10, 5, 3) = 2
 
 
 //type of elements in array can be any type which has some associative function
@@ -124,10 +124,10 @@ let stringArray = ["a", "b", "c", "A", "B", "C"]
 
 let stringSegmentTree = SegmentTree(array: stringArray, function: +)
 
-print(stringSegmentTree.query(withLeftBound: 0, rightBound: 1)) // "a"+"b" = "ab"
-print(stringSegmentTree.query(withLeftBound: 2, rightBound: 3)) // "c"+"A" = "cA"
-print(stringSegmentTree.query(withLeftBound: 1, rightBound: 3)) // "b"+"c"+"A" = "bcA"
-print(stringSegmentTree.query(withLeftBound: 0, rightBound: 5)) // "a"+"b"+"c"+"A"+"B"+"C" = "abcABC"
+print(stringSegmentTree.query(leftBound: 0, rightBound: 1)) // "a"+"b" = "ab"
+print(stringSegmentTree.query(leftBound: 2, rightBound: 3)) // "c"+"A" = "cA"
+print(stringSegmentTree.query(leftBound: 1, rightBound: 3)) // "b"+"c"+"A" = "bcA"
+print(stringSegmentTree.query(leftBound: 0, rightBound: 5)) // "a"+"b"+"c"+"A"+"B"+"C" = "abcABC"
 
 stringSegmentTree.replaceItem(at: 0, withItem: "I")
 stringSegmentTree.replaceItem(at: 1, withItem: " like")
@@ -136,4 +136,4 @@ stringSegmentTree.replaceItem(at: 3, withItem: " and")
 stringSegmentTree.replaceItem(at: 4, withItem: " swift")
 stringSegmentTree.replaceItem(at: 5, withItem: "!")
 
-print(stringSegmentTree.query(withLeftBound: 0, rightBound: 5))
+print(stringSegmentTree.query(leftBound: 0, rightBound: 5))
