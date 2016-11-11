399aadf2d11f45ef1360fdcf72f5f7eda852431b
Rangding Zhang <love-nankai@163.com>
2016-11-11 11:03:54 +0800

    Update Contents.swift

--- Binary Tree/BinaryTree.playground/Contents.swift
+++ Binary Tree/BinaryTree.playground/Contents.swift
@@ -1,32 +1,3 @@
-//: Playground - noun: a place where people can play
-
-public indirect enum BinaryTree<T> {
-  case Node(BinaryTree<T>, T, BinaryTree<T>)
-  case Empty
-
-  public var count: Int {
-    switch self {
-    case let .Node(left, _, right):
-      return left.count + 1 + right.count
-    case .Empty:
-      return 0
-    }
-  }
-}
-
-extension BinaryTree: CustomStringConvertible {
-  public var description: String {
-    switch self {
-    case let .Node(left, value, right):
-      return "value: \(value), left = [" + left.description + "], right = [" + right.description + "]"
-    case .Empty:
-      return ""
-    }
-  }
-}
-
-
-
 // leaf nodes
 let node5 = BinaryTree.Node(.Empty, "5", .Empty)
 let nodeA = BinaryTree.Node(.Empty, "a", .Empty)
@@ -50,34 +21,6 @@ let tree = BinaryTree.Node(timesLeft, "+", timesRight)
 print(tree)
 tree.count  // 12
 
-
-
-extension BinaryTree {
-  public func traverseInOrder(process: (T) -> Void) {
-    if case let .Node(left, value, right) = self {
-      left.traverseInOrder(process: process)
-      process(value)
-      right.traverseInOrder(process: process)
-    }
-  }
-
-  public func traversePreOrder(process: (T) -> Void) {
-    if case let .Node(left, value, right) = self {
-      process(value)
-      left.traversePreOrder(process: process)
-      right.traversePreOrder(process: process)
-    }
-  }
-
-  public func traversePostOrder(process: (T) -> Void) {
-    if case let .Node(left, value, right) = self {
-      left.traversePostOrder(process: process)
-      right.traversePostOrder(process: process)
-      process(value)
-    }
-  }
-}
-
-//tree.traverseInOrder { s in print(s) }
+tree.traverseInOrder { s in print(s) }
 //tree.traversePreOrder { s in print(s) }
-tree.traversePostOrder { s in print(s) }
+//tree.traversePostOrder { s in print(s) }
