45b02ee04408cb2ef7ae3d82c5d5e5c4ffc9c617
Rangding Zhang <love-nankai@163.com>
2016-11-11 10:53:33 +0800

    Update Contents.swift

--- Binary Search Tree/Solution 2/BinarySearchTree.playground/Contents.swift
+++ Binary Search Tree/Solution 2/BinarySearchTree.playground/Contents.swift
@@ -1,13 +1,7 @@
-var tree = BinarySearchTree.Leaf(7)
-tree.insert(2)
-tree.insert(5)
-tree.insert(10)
-tree.insert(9)
-tree.insert(1)
+//var tree = BinarySearchTree(7)
+var tree = BinarySearchTree(array: [2, 5, 10, 9, 1])
 print(tree)
 
 tree.search(10)
 tree.search(1)
 tree.search(11)
-
-
