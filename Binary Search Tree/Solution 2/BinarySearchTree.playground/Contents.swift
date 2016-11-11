67b8a6a88f73f7c306c1616d47ad71702eac63c7
Rangding Zhang <love-nankai@163.com>
2016-11-11 10:29:26 +0800

    Update Contents.swift

--- Binary Search Tree/Solution 2/BinarySearchTree.playground/Contents.swift
+++ Binary Search Tree/Solution 2/BinarySearchTree.playground/Contents.swift
@@ -1,16 +1,13 @@
-//: Playground - noun: a place where people can play
-
-// Each time you insert something, you get back a completely new tree.
 var tree = BinarySearchTree.Leaf(7)
-tree = tree.insert(newValue: 2)
-tree = tree.insert(newValue: 5)
-tree = tree.insert(newValue: 10)
-tree = tree.insert(newValue: 9)
-tree = tree.insert(newValue: 1)
+tree.insert(2)
+tree.insert(5)
+tree.insert(10)
+tree.insert(9)
+tree.insert(1)
 print(tree)
 
-tree.search(x: 10)
-tree.search(x: 1)
-tree.search(x: 11)
+tree.search(10)
+tree.search(1)
+tree.search(11)
 
 
