9a69d4852b7e415b989a0da9139f6d4d33825f6e
jsbean <jbean46@gmail.com>
2016-11-23 22:55:02 -0500

    Break up super long comment

--- Minimum Edit Distance/MinimumEditDistance.playground/Contents.swift
+++ Minimum Edit Distance/MinimumEditDistance.playground/Contents.swift
@@ -23,7 +23,8 @@ extension String {
                     // substitution of equal symbols with cost 0
                     matrix[i + 1][j + 1] = matrix[i][j]
                 } else {
-                    // minimum of the cost of insertion, deletion, or substitution added to the already computed costs in the corresponding cells
+                    // minimum of the cost of insertion, deletion, or substitution 
+                    // added to the already computed costs in the corresponding cells
                     matrix[i + 1][j + 1] = min(matrix[i][j] + 1, matrix[i + 1][j] + 1, matrix[i][j + 1] + 1)
                 }
             }
