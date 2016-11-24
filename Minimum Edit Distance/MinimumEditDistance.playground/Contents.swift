cd9e426d425f0565d2fff9a33f2cfcca4565d50d
jsbean <jbean46@gmail.com>
2016-11-23 22:49:25 -0500

    Add some breathing room

--- Minimum Edit Distance/MinimumEditDistance.playground/Contents.swift
+++ Minimum Edit Distance/MinimumEditDistance.playground/Contents.swift
@@ -3,17 +3,17 @@ extension String {
     public func minimumEditDistance(other: String) -> Int {
         let m = self.characters.count
         let n = other.characters.count
-        var matrix = [[Int]](repeating: [Int](repeating: 0, count: n+1), count: m+1)
+        var matrix = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: m + 1)
         
         // initialize matrix
         for index in 1...m {
             // the distance of any first string to an empty second string
-            matrix[index][0]=index
+            matrix[index][0] = index
         }
         
         for index in 1...n {
             // the distance of any second string to an empty first string
-            matrix[0][index]=index
+            matrix[0][index] = index
         }
         
         // compute Levenshtein distance
@@ -21,10 +21,10 @@ extension String {
             for (j, otherChar) in other.characters.enumerated() {
                 if otherChar == selfChar {
                     // substitution of equal symbols with cost 0
-                    matrix[i+1][j+1] = matrix[i][j]
+                    matrix[i + 1][j + 1] = matrix[i][j]
                 } else {
                     // minimum of the cost of insertion, deletion, or substitution added to the already computed costs in the corresponding cells
-                    matrix[i+1][j+1] = min(matrix[i][j]+1, matrix[i+1][j]+1, matrix[i][j+1]+1)
+                    matrix[i + 1][j + 1] = min(matrix[i][j] + 1, matrix[i + 1][j] + 1, matrix[i][j + 1] + 1)
                 }
             }
         }
