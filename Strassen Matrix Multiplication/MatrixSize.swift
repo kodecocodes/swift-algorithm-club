1e23b7f859ed83e21dd37941113102af0d8c4d28
Richard <r.ash8347@gmail.com>
2016-12-13 14:30:07 -0800

    Code updates

--- Strassen Matrix Multiplication/MatrixSize.swift
+++ Strassen Matrix Multiplication/MatrixSize.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct MatrixSize: Equatable {
+  let rows: Int
+  let columns: Int
+  
+  func forEach(_ body: (Int, Int) throws -> Void) rethrows {
+    for row in 0..<rows {
+      for column in 0..<columns {
+        try? body(row, column)
+      }
+    }
+  }
+}
+
+func ==(lhs: MatrixSize, rhs: MatrixSize) -> Bool {
+  return lhs.columns == rhs.columns && lhs.rows == rhs.rows
+}
--- Strassen Matrix Multiplication/Number.swift
+++ Strassen Matrix Multiplication/Number.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+protocol Number: Multipliable, Addable {
+  static var zero: Self { get }
+}
+
+protocol Addable {
+  static func +(lhs: Self, rhs: Self) -> Self
+  static func -(lhs: Self, rhs: Self) -> Self
+}
+
+protocol Multipliable {
+  static func *(lhs: Self, rhs: Self) -> Self
+}
+
+extension Int: Number {
+  static var zero: Int {
+    return 0
+  }
+}
+
+extension Double: Number {
+  static var zero: Double {
+    return 0.0
+  }
+}
+
+extension Float: Number {
+  static var zero: Float {
+    return 0.0
+  }
+}
+
+
+extension Array where Element: Number {
+  func dot(_ b: Array<Element>) -> Element {
+    let a = self
+    assert(a.count == b.count, "Can only take the dot product of arrays of the same length!")
+    let c = a.indices.map{ a[$0] * b[$0] }
+    return c.reduce(Element.zero, { $0 + $1 })
+  }
+}
