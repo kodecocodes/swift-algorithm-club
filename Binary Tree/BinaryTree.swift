31bc4c3159ea7574ee557ad46dbb65be33987395
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-12-15 08:52:41 +0900

    Use lower camel case in enum case

--- Binary Tree/BinaryTree.swift
+++ Binary Tree/BinaryTree.swift
@@ -4,14 +4,14 @@
   Nodes don't have a reference to their parent.
 */
 public indirect enum BinaryTree<T> {
-  case Node(BinaryTree<T>, T, BinaryTree<T>)
-  case Empty
+  case node(BinaryTree<T>, T, BinaryTree<T>)
+  case empty
 
   public var count: Int {
     switch self {
-    case let .Node(left, _, right):
+    case let .node(left, _, right):
       return left.count + 1 + right.count
-    case .Empty:
+    case .empty:
       return 0
     }
   }
@@ -20,9 +20,9 @@ public indirect enum BinaryTree<T> {
 extension BinaryTree: CustomStringConvertible {
   public var description: String {
     switch self {
-    case let .Node(left, value, right):
+    case let .node(left, value, right):
       return "value: \(value), left = [" + left.description + "], right = [" + right.description + "]"
-    case .Empty:
+    case .empty:
       return ""
     }
   }
@@ -30,7 +30,7 @@ extension BinaryTree: CustomStringConvertible {
 
 extension BinaryTree {
   public func traverseInOrder(@noescape process: T -> Void) {
-    if case let .Node(left, value, right) = self {
+    if case let .node(left, value, right) = self {
       left.traverseInOrder(process)
       process(value)
       right.traverseInOrder(process)
@@ -38,7 +38,7 @@ extension BinaryTree {
   }
 
   public func traversePreOrder(@noescape process: T -> Void) {
-    if case let .Node(left, value, right) = self {
+    if case let .node(left, value, right) = self {
       process(value)
       left.traversePreOrder(process)
       right.traversePreOrder(process)
@@ -46,7 +46,7 @@ extension BinaryTree {
   }
 
   public func traversePostOrder(@noescape process: T -> Void) {
-    if case let .Node(left, value, right) = self {
+    if case let .node(left, value, right) = self {
       left.traversePostOrder(process)
       right.traversePostOrder(process)
       process(value)
@@ -56,10 +56,10 @@ extension BinaryTree {
 
 extension BinaryTree {
   func invert() -> BinaryTree {
-    if case let .Node(left, value, right) = self {
-      return .Node(right.invert(), value, left.invert())
+    if case let .node(left, value, right) = self {
+      return .node(right.invert(), value, left.invert())
     } else {
-      return .Empty
+      return .empty
     }
   }
 }
