78e78a4b9e81b57698d95e68d8abbd86c218ef95
mmazzei <matiasmazzei@gmail.com>
2016-12-24 14:42:54 -0300

    Fix swiftlint warnings.

--- Boyer-Moore/Tests/BoyerMooreTests.swift
+++ Boyer-Moore/Tests/BoyerMooreTests.swift
@@ -30,48 +30,48 @@ class BoyerMooreTest: XCTestCase {
         let match = string.substring(with: startIndex..<endIndex)
         XCTAssertEqual(match, pattern)
     }
-    
+
     func testSearchTheSameString() {
         let string = "ABCDEF"
         let pattern = "ABCDEF"
         assert(pattern: pattern, existsIn: string)
     }
-    
+
     func testSearchAPreffix() {
         let string = "ABCDEFGHIJK"
         let pattern = "ABCDEF"
         assert(pattern: pattern, existsIn: string)
     }
-    
+
     func testSearchASuffix() {
         let string = "ABCDEFGHIJK"
         let pattern = "HIJK"
         assert(pattern: pattern, existsIn: string)
     }
-    
+
     func testSearchAStringFromTheMiddle() {
         let string = "ABCDEFGHIJK"
         let pattern = "EFG"
         assert(pattern: pattern, existsIn: string)
     }
-    
-    
+
     func testSearchInvalidPattern() {
         let string = "ABCDEFGHIJK"
         let pattern = "AEF"
         assert(pattern: pattern, doesNotExistsIn: string)
     }
-    
+
     func testSearchPatternWithDuplicatedCharacter() {
-        let string = "Goal: Write a string search algorithm in pure Swift without importing Foundation or using NSString's rangeOfString() method."
+        let string = "Goal: Write a string search algorithm in pure Swift "
+                     + "without importing Foundation or using NSString's rangeOfString() method."
         let pattern = "NSS"
         assert(pattern: pattern, existsIn: string)
     }
 
     func testSearchInvalidPatternWithDuplicatedCharacter() {
-        let string = "Goal: Write a string search algorithm in pure Swift without importing Foundation or using NSString's rangeOfString() method."
+        let string = "Goal: Write a string search algorithm in pure Swift "
+            + "without importing Foundation or using NSString's rangeOfString() method."
         let pattern = "nss"
         assert(pattern: pattern, doesNotExistsIn: string)
     }
-    
 }
