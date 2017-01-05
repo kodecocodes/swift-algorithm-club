797a2e9e2bf70911222cac674e05841b80679310
iamsimranjot <iamsimranjot@gmail.com>
2017-01-06 02:56:13 +0530

    Updated Merge Sort (Recursive) to Generic.

--- Merge Sort/MergeSort.swift
+++ Merge Sort/MergeSort.swift
@@ -6,45 +6,45 @@
 //
 //
 
-func mergeSort(_ array: [Int]) -> [Int] {
-  guard array.count > 1 else { return array }
-  let middleIndex = array.count / 2
-  let leftArray = mergeSort(Array(array[0..<middleIndex]))
-  let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
-  return merge(leftPile: leftArray, rightPile: rightArray)
+func mergeSort<T: Comparable>(_ array: [T]) -> [T] {
+    guard array.count > 1 else { return array }
+    let middleIndex = array.count / 2
+    let leftArray = mergeSort(Array(array[0..<middleIndex]))
+    let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+    return merge(leftPile: leftArray, rightPile: rightArray)
 }
 
-func merge(leftPile: [Int], rightPile: [Int]) -> [Int] {
-  var leftIndex = 0
-  var rightIndex = 0
-  var orderedPile = [Int]()
-
-  while leftIndex < leftPile.count && rightIndex < rightPile.count {
-    if leftPile[leftIndex] < rightPile[rightIndex] {
-      orderedPile.append(leftPile[leftIndex])
-      leftIndex += 1
-    } else if leftPile[leftIndex] > rightPile[rightIndex] {
-      orderedPile.append(rightPile[rightIndex])
-      rightIndex += 1
-    } else {
-      orderedPile.append(leftPile[leftIndex])
-      leftIndex += 1
-      orderedPile.append(rightPile[rightIndex])
-      rightIndex += 1
+func merge<T: Comparable>(leftPile: [T], rightPile: [T]) -> [T] {
+    var leftIndex = 0
+    var rightIndex = 0
+    var orderedPile = [T]()
+    
+    while leftIndex < leftPile.count && rightIndex < rightPile.count {
+        if leftPile[leftIndex] < rightPile[rightIndex] {
+            orderedPile.append(leftPile[leftIndex])
+            leftIndex += 1
+        } else if leftPile[leftIndex] > rightPile[rightIndex] {
+            orderedPile.append(rightPile[rightIndex])
+            rightIndex += 1
+        } else {
+            orderedPile.append(leftPile[leftIndex])
+            leftIndex += 1
+            orderedPile.append(rightPile[rightIndex])
+            rightIndex += 1
+        }
     }
-  }
-
-  while leftIndex < leftPile.count {
-    orderedPile.append(leftPile[leftIndex])
-    leftIndex += 1
-  }
-
-  while rightIndex < rightPile.count {
-    orderedPile.append(rightPile[rightIndex])
-    rightIndex += 1
-  }
-
-  return orderedPile
+    
+    while leftIndex < leftPile.count {
+        orderedPile.append(leftPile[leftIndex])
+        leftIndex += 1
+    }
+    
+    while rightIndex < rightPile.count {
+        orderedPile.append(rightPile[rightIndex])
+        rightIndex += 1
+    }
+    
+    return orderedPile
 }
 
 /*
