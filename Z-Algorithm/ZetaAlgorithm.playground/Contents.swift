cf0d1b539b2bf628a76e37b33332bab02c9bcc1a
Kelvin Lau <kelvin.kl.lau@gmail.com>
2017-01-16 10:59:20 -0800

    Modified Z-Algorithm to conform to our style guide.

--- Z-Algorithm/ZetaAlgorithm.playground/Contents.swift
+++ Z-Algorithm/ZetaAlgorithm.playground/Contents.swift
@@ -2,88 +2,88 @@
 
 
 func ZetaAlgorithm(ptrn: String) -> [Int]? {
-
-    let pattern = Array(ptrn.characters)
-    let patternLength: Int = pattern.count
-
-    guard patternLength > 0 else {
-        return nil
-    }
-
-    var zeta: [Int] = [Int](repeating: 0, count: patternLength)
-
-    var left: Int = 0
-    var right: Int = 0
-    var k_1: Int = 0
-    var betaLength: Int = 0
-    var textIndex: Int = 0
-    var patternIndex: Int = 0
-
-    for k in 1 ..< patternLength {
-        if k > right {
-            patternIndex = 0
-
-            while k + patternIndex < patternLength  &&
-                pattern[k + patternIndex] == pattern[patternIndex] {
-                patternIndex = patternIndex + 1
-            }
-
-            zeta[k] = patternIndex
-
-            if zeta[k] > 0 {
-                left = k
-                right = k + zeta[k] - 1
-            }
-        } else {
-            k_1 = k - left + 1
-            betaLength = right - k + 1
-
-            if zeta[k_1 - 1] < betaLength {
-                zeta[k] = zeta[k_1 - 1]
-            } else if zeta[k_1 - 1] >= betaLength {
-                textIndex = betaLength
-                patternIndex = right + 1
-
-                while patternIndex < patternLength && pattern[textIndex] == pattern[patternIndex] {
-                    textIndex = textIndex + 1
-                    patternIndex = patternIndex + 1
-                }
-
-                zeta[k] = patternIndex - k
-                left = k
-                right = patternIndex - 1
-            }
+  
+  let pattern = Array(ptrn.characters)
+  let patternLength = pattern.count
+  
+  guard patternLength > 0 else {
+    return nil
+  }
+  
+  var zeta = [Int](repeating: 0, count: patternLength)
+  
+  var left = 0
+  var right = 0
+  var k_1 = 0
+  var betaLength = 0
+  var textIndex = 0
+  var patternIndex = 0
+  
+  for k in 1 ..< patternLength {
+    if k > right {
+      patternIndex = 0
+      
+      while k + patternIndex < patternLength  &&
+        pattern[k + patternIndex] == pattern[patternIndex] {
+          patternIndex = patternIndex + 1
+      }
+      
+      zeta[k] = patternIndex
+      
+      if zeta[k] > 0 {
+        left = k
+        right = k + zeta[k] - 1
+      }
+    } else {
+      k_1 = k - left + 1
+      betaLength = right - k + 1
+      
+      if zeta[k_1 - 1] < betaLength {
+        zeta[k] = zeta[k_1 - 1]
+      } else if zeta[k_1 - 1] >= betaLength {
+        textIndex = betaLength
+        patternIndex = right + 1
+        
+        while patternIndex < patternLength && pattern[textIndex] == pattern[patternIndex] {
+          textIndex = textIndex + 1
+          patternIndex = patternIndex + 1
         }
+        
+        zeta[k] = patternIndex - k
+        left = k
+        right = patternIndex - 1
+      }
     }
-    return zeta
+  }
+  return zeta
 }
 
 
 extension String {
-
-    func indexesOf(pattern: String) -> [Int]? {
-        let patternLength: Int = pattern.characters.count
-        let zeta = ZetaAlgorithm(ptrn: pattern + "💲" + self)
-
-        guard zeta != nil else {
-            return nil
-        }
-
-        var indexes: [Int] = [Int]()
-
-        /* Scan the zeta array to find matched patterns */
-        for i in 0 ..< zeta!.count {
-            if zeta![i] == patternLength {
-                indexes.append(i - patternLength - 1)
-            }
-        }
-
-        guard !indexes.isEmpty else {
-            return nil
-        }
-
-        return indexes
+  
+  func indexesOf(pattern: String) -> [Int]? {
+    let patternLength = pattern.characters.count
+    let zeta = ZetaAlgorithm(ptrn: pattern + "💲" + self)
+    
+    guard zeta != nil else {
+      return nil
     }
+    
+    var indexes: [Int] = []
+    
+    /* Scan the zeta array to find matched patterns */
+    for i in 0 ..< zeta!.count {
+      if zeta![i] == patternLength {
+        indexes.append(i - patternLength - 1)
+      }
+    }
+    
+    guard !indexes.isEmpty else {
+      return nil
+    }
+    
+    return indexes
+  }
 }
 
 /* Examples */
