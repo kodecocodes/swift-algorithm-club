69cb42eef48c610f2f42e6c5d51781fdb3482e6a
橘子 <726451484@qq.com>
2017-03-01 12:13:54 +0800

    Update ShellSortExample.swift
    let code to suit style guide

--- Shell Sort/ShellSortExample.swift
+++ Shell Sort/ShellSortExample.swift
@@ -8,23 +8,23 @@
 
 import Foundation
 
-public func shellSort(_ list : inout [Int])
-{
-    var sublistCount = list.count / 2
+public func shellSort(_ list: inout [Int]) {
     
-    while sublistCount > 0
-    {
-        for index in 0..<list.count{
-            
+    var sublistCount = list.count / 2
+   
+    while sublistCount > 0 {
+        
+        for index in 0..<list.count {
+           
             guard index + sublistCount < list.count else { break }
             
-            if list[index] > list[index + sublistCount]{
+            if list[index] > list[index + sublistCount] {
                 swap(&list[index], &list[index + sublistCount])
             }
             
             guard sublistCount == 1 && index > 0 else { continue }
             
-            if list[index - 1] > list[index]{
+            if list[index - 1] > list[index] {
                 swap(&list[index - 1], &list[index])
             }
         }
