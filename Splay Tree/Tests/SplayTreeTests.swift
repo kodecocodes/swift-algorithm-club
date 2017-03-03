cc973dcb62d310f1af0d08d408c782a57e3523cb
barbara <barbara@komoot.de>
2017-03-03 21:01:11 +0100

    Fixes for connecting nodes during rotations

--- Splay Tree/Tests/SplayTreeTests.swift
+++ Splay Tree/Tests/SplayTreeTests.swift
@@ -1,19 +1,30 @@
 import XCTest
 
 class SplayTreeTests: XCTestCase {
-  var tree: SplayTree<Int>!
-  
-  override func setUp() {
-    super.setUp()
-    tree = SplayTree<Int>(value: 1)
-  }
-  
-  func testElements() {
-    print(tree)
-    let tree1 = tree.insert(value: 10)
-    print(tree1!)
-    let tree2 = tree1!.insert(value: 2)
-    print(tree2!)
-  }
-
+   
+    var tree: SplayTree<Int>!
+    var tree1: SplayTree<Int>!
+    
+    override func setUp() {
+        super.setUp()
+        tree = SplayTree<Int>(value: 1)
+        tree1 = tree.insert(value: 10)?.insert(value: 20)?.insert(value: 3)//?.insert(value: 6)?.insert(value: 100)?.insert(value: 44)
+    }
+    
+    func testInsertion() {
+        let tree1 = tree.insert(value: 10)
+        assert(tree1?.root?.value == 10)
+        
+        let tree2 = tree1!.insert(value: 2)
+        assert(tree2?.root?.value == 2)
+    }
+    
+    
+    func testDeleteExisting() {
+        print(tree1)
+        let tree2 = tree1.search(value: 6)?.remove()
+        
+    }
+    
+    
 }
