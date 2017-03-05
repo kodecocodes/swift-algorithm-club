ecba738935641768526277d908153adc3f9d8ca6
barbara <barbara@komoot.de>
2017-03-05 19:38:55 +0100

    Playground Splay Tree

--- Splay Tree/SplayTree.playground/Contents.swift
+++ Splay Tree/SplayTree.playground/Contents.swift
@@ -1,7 +1,29 @@
 //: Playground - Splay Tree Implementation
 
 
-let splayTree = SplayTree(array: [1])
+let splayTree = SplayTree(value: 1)
 splayTree.insert(value: 2)
 splayTree.insert(value: 10)
 splayTree.insert(value: 6)
+
+splayTree.remove(value: 10)
+splayTree.remove(value: 6)
+
+splayTree.insert(value: 55)
+splayTree.insert(value: 559)
+splayTree.remove(value: 2)
+splayTree.remove(value: 1)
+splayTree.remove(value: 55)
+splayTree.remove(value: 559)
+
+splayTree.insert(value: 1843000)
+splayTree.insert(value: 1238)
+splayTree.insert(value: -1)
+splayTree.insert(value: 87)
+
+splayTree.minimum()
+splayTree.maximum()
+
+
+
+
