0bd5ea7b0211f7c56d3f60f074815a1eff43263a
new_user_name <new@email.com>
2017-03-09 23:44:35 +0600

    Variable Names Were Changed to camelCase

--- Dynamic Programming Knapsack /Dynamic Programming Knapsack.playground/Contents.swift
+++ Dynamic Programming Knapsack /Dynamic Programming Knapsack.playground/Contents.swift
@@ -1,36 +1,36 @@
 import Foundation
 
-var number_of_items = Int()
-var capacity_of_bag = Int()
-var weights_items = [Int]()
-var values_items = [Int]()
+var numberOfItems = Int()
+var capacityOfBag = Int()
+var items = [Int]()
+var values = [Int]()
 var dp = [[Int]]()
 
 func knapsack() -> Int {
-    dp = Array(repeating: Array(repeating: 0, count: capacity_of_bag+1),
-               count: number_of_items+1)
-    for i in 0...number_of_items {
-        for j in 0...capacity_of_bag {
+    dp = Array(repeating: Array(repeating: 0, count: capacityOfBag+1),
+               count: numberOfItems+1)
+    for i in 0...numberOfItems {
+        for j in 0...capacityOfBag {
             if i == 0 || j == 0 {
                 continue
             }
-            if j<weights_items[i] {
+            if j<items[i] {
                 dp[i][j] = dp[i-1][j]
             } else {
-                dp[i][j] = max(values_items[i] + dp[i-1][j-weights_items[i]], dp[i-1][j])
+                dp[i][j] = max(values[i] + dp[i-1][j-items[i]], dp[i-1][j])
             }
         }
     }
-    return dp[number_of_items][capacity_of_bag]
+    return dp[numberOfItems][capacityOfBag]
 }
 
 func solution() {
     
-    capacity_of_bag = 4
-    number_of_items = 5
+    capacityOfBag = 4
+    numberOfItems = 5
     
-    weights_items = [0, 1, 2, 3, 2, 2]
-    values_items = [0, 8, 4, 0, 5, 3]
+    items = [0, 1, 2, 3, 2, 2]
+    values = [0, 8, 4, 0, 5, 3]
     
     let result = knapsack()
     
