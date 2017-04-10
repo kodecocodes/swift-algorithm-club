a613ca599c03a98e588e32982d57097d5b388909
Marwan Alani <marwan@alani.ca>
2017-04-10 16:39:50 -0400

    Updated the Playground file with more comments and added an overloadded implementations of 'insert' & 'append' to accept a whole list as an argument

--- Linked List/LinkedList.playground/Contents.swift
+++ Linked List/LinkedList.playground/Contents.swift
@@ -1,30 +1,41 @@
-//: Playground - noun: a place where people can play
+//: # Linked Lists
 
-public class LinkedListNode<T> {
-  var value: T
-  var next: LinkedListNode?
-  weak var previous: LinkedListNode?
-  
-  public init(value: T) {
-    self.value = value
-  }
-}
+// For best results, don't forget to select "Show Rendered Markup" from XCode's "Editor" menu
 
+//: Linked List Class Declaration:
 public final class LinkedList<T> {
+  
+  // Linked List's Node Class Declaration
+  public class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    weak var previous: LinkedListNode?
+    
+    public init(value: T) {
+      self.value = value
+    }
+  }
+  
+  // Typealiasing the node class to increase readability of code
   public typealias Node = LinkedListNode<T>
   
+  // The head of the Linked List
   fileprivate var head: Node?
   
+  // Default initializer
   public init() {}
   
+  // Computed property to check if the linked list is empty
   public var isEmpty: Bool {
     return head == nil
   }
   
+  // Computed property to get the first node in the linked list (if any)
   public var first: Node? {
     return head
   }
   
+  // Computed property to iterate through the linked list and return the last node in the list (if any)
   public var last: Node? {
     if var node = head {
       while case let next? = node.next {
@@ -36,6 +47,7 @@ public final class LinkedList<T> {
     }
   }
   
+  // Computed property to iterate through the linked list and return the total number of nodes
   public var count: Int {
     if var node = head {
       var c = 1
@@ -49,6 +61,7 @@ public final class LinkedList<T> {
     }
   }
   
+  // Function to return the node at a specific index
   public func node(atIndex index: Int) -> Node? {
     if index >= 0 {
       var node = head
@@ -62,17 +75,20 @@ public final class LinkedList<T> {
     return nil
   }
   
+  // Subscript function to return the node at a specific index
   public subscript(index: Int) -> T {
     let node = self.node(atIndex: index)
     assert(node != nil)
     return node!.value
   }
   
+  // Append a value
   public func append(_ value: T) {
     let newNode = Node(value: value)
     self.append(newNode)
   }
   
+  // Append a node without taking ownership (appends a copy)
   public func append(_ node: Node) {
     let newNode = LinkedListNode(value: node.value)
     if let lastNode = last {
@@ -83,6 +99,16 @@ public final class LinkedList<T> {
     }
   }
   
+  // Append a whole linked list without taking ownership (appends a copy)
+  public func append(_ list: LinkedList) {
+    var nodeToCopy = list.head
+    while let node = nodeToCopy {
+      self.append(node.value)
+      nodeToCopy = node.next
+    }
+  }
+  
+  // Private helper function to return the nodes (in a tuple) before and after a specific index
   private func nodesBeforeAndAfter(index: Int) -> (Node?, Node?) {
     assert(index >= 0)
     
@@ -100,11 +126,13 @@ public final class LinkedList<T> {
     return (prev, next)
   }
   
+  // Insert a value at a specific index
   public func insert(_ value: T, atIndex index: Int) {
     let newNode = Node(value: value)
     self.insert(newNode, atIndex: index)
   }
   
+  // Insert a node at a specific index without taking ownership (inserts a copy)
   public func insert(_ node: Node, atIndex index: Int) {
     let (prev, next) = nodesBeforeAndAfter(index: index)
     let newNode = LinkedListNode(value: node.value)
@@ -118,10 +146,33 @@ public final class LinkedList<T> {
     }
   }
   
+  // Insert a whole linked list at a specific index without taking ownership (inserts a copy)
+  public func insert(_ list: LinkedList, atIndex index: Int) {
+    if list.isEmpty { return }
+    var (prev, next) = nodesBeforeAndAfter(index: index)
+    var nodeToCopy = list.head
+    var newNode:Node?
+    while let node = nodeToCopy {
+      newNode = Node(value: node.value)
+      newNode?.previous = prev
+      if let previous = prev {
+        previous.next = newNode
+      } else {
+        self.head = newNode
+      }
+      nodeToCopy = nodeToCopy?.next
+      prev = newNode
+    }
+    prev?.next = next
+    next?.previous = prev
+  }
+  
+  // Function to remove all nodes from the list
   public func removeAll() {
     head = nil
   }
   
+  // Function to remove a specific node from the list and return its value
   @discardableResult public func remove(node: Node) -> T {
     let prev = node.previous
     let next = node.next
@@ -138,11 +189,13 @@ public final class LinkedList<T> {
     return node.value
   }
   
+  // Function to remove the last node in the list
   @discardableResult public func removeLast() -> T {
     assert(!isEmpty)
     return remove(node: last!)
   }
   
+  // Function to remove the node at a specific index from the list
   @discardableResult public func remove(atIndex index: Int) -> T {
     let node = self.node(atIndex: index)
     assert(node != nil)
@@ -150,6 +203,7 @@ public final class LinkedList<T> {
   }
 }
 
+//: End of the base class declarations & beginning of extensions' declarations:
 extension LinkedList: CustomStringConvertible {
   public var description: String {
     var s = "["
@@ -218,6 +272,8 @@ extension LinkedList: ExpressibleByArrayLiteral {
   }
 }
 
+
+//: Ok, now that the declarations are done, let's see our Linked List in action:
 let list = LinkedList<String>()
 list.isEmpty                  // true
 list.first                    // nil
@@ -247,10 +303,20 @@ list[0]     // "Hello"
 list[1]     // "World"
 //list[2]   // crash!
 
+let list2 = LinkedList<String>()
+list2.append("Goodbye")
+list2.append("World")
+list.append(list2)            // [Hello, World, Goodbye, World]
+list2.removeAll()             // [ ]
+list2.isEmpty                 // true
+list.removeLast()             // "World"
+list.remove(atIndex: 2)       // "Goodbye"
+
+
 list.insert("Swift", atIndex: 1)
-list[0]
-list[1]
-list[2]
+list[0]     // "Hello"
+list[1]     // "Swift"
+list[2]     // "World"
 print(list)
 
 list.reverse()   // [World, Swift, Hello]
@@ -261,9 +327,6 @@ let m = list.map { s in s.characters.count }
 m    // [8, 6, 5]
 let f = list.filter { s in s.characters.count > 5 }
 f    // [Universe, Swifty]
-
-//list.removeAll()
-//list.isEmpty
 
 list.remove(node: list.first!) // "Universe"
 list.count                     // 2
@@ -286,3 +349,4 @@ let listArrayLiteral2: LinkedList = ["Swift", "Algorithm", "Club"]
 listArrayLiteral2.count        // 3
 listArrayLiteral2[0]           // "Swift"
 listArrayLiteral2.removeLast()  // "Club"
+
