3bdb63de34cba4abcc581980c83da650bc8d3ba4
Taras Nikulin <crabman@yandex-team.ru>
2017-04-17 12:17:45 +0300

    Hashable, Equatable to Extensions

--- Dijkstra Algorithm/Dijkstra.playground/Sources/Vertex.swift
+++ Dijkstra Algorithm/Dijkstra.playground/Sources/Vertex.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-open class Vertex: Hashable, Equatable {
+open class Vertex {
 
     open var identifier: String!
     open var neighbors: [(Vertex, Double)] = []
@@ -11,19 +11,23 @@ open class Vertex: Hashable, Equatable {
         self.identifier = identifier
     }
 
+    open func clearCache() {
+        self.pathLengthFromStart = Double.infinity
+        self.pathVerticesFromStart = []
+    }
+}
+
+extension Vertex: Hashable {
     open var hashValue: Int {
         return self.identifier.hashValue
     }
+}
 
+extension Vertex: Equatable {
     public static func ==(lhs: Vertex, rhs: Vertex) -> Bool {
         if lhs.hashValue == rhs.hashValue {
             return true
         }
         return false
-    }
-
-    open func clearCache() {
-        self.pathLengthFromStart = Double.infinity
-        self.pathVerticesFromStart = []
     }
 }
