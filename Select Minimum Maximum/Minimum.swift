183815a72d83885fea57fb6fa8e316c6f43f5257
TheIronBorn <TheIronBorn@users.noreply.github.com>
2017-06-04 08:50:01 -0700

    implement guarded array first

--- Select Minimum Maximum/Minimum.swift
+++ Select Minimum Maximum/Minimum.swift
@@ -3,11 +3,8 @@
  */
 
 func minimum<T: Comparable>(_ array: [T]) -> T? {
-    guard !array.isEmpty else {
-        return nil
-    }
-
-    var minimum = array.first!
+    guard minimum = array.first else { return nil }
+ 
     for element in array.dropFirst() {
         minimum = element < minimum ? element : minimum
     }
