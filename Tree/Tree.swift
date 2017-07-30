e36e7119714e164c0997eb1365334a4fa6979346
ck <remlostime@gmail.com>
2017-07-30 07:56:08 -0700

    [Swift 4] Update Tree

--- Tree/Tree.swift
+++ Tree/Tree.swift
@@ -1,39 +1,40 @@
 public class TreeNode<T> {
-  public var value: T
+    public var value: T
 
-  public weak var parent: TreeNode?
-  public var children = [TreeNode<T>]()
+    public weak var parent: TreeNode?
+    public var children = [TreeNode<T>]()
 
-  public init(value: T) {
-    self.value = value
-  }
+    public init(value: T) {
+        self.value = value
+    }
 
-  public func addChild(_ node: TreeNode<T>) {
-    children.append(node)
-    node.parent = self
-  }
+    public func addChild(_ node: TreeNode<T>) {
+        children.append(node)
+        node.parent = self
+    }
 }
 
 extension TreeNode: CustomStringConvertible {
-  public var description: String {
-    var s = "\(value)"
-    if !children.isEmpty {
-      s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+    public var description: String {
+        var s = "\(value)"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
     }
-    return s
-  }
 }
 
 extension TreeNode where T: Equatable {
-  public func search(_ value: T) -> TreeNode? {
-    if value == self.value {
-      return self
+    public func search(_ value: T) -> TreeNode? {
+        if value == self.value {
+            return self
+        }
+        for child in children {
+            if let found = child.search(value) {
+                return found
+            }
+        }
+        return nil
     }
-    for child in children {
-      if let found = child.search(value) {
-        return found
-      }
-    }
-    return nil
-  }
 }
+
