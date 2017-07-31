aad5d28451f271dcf4f47e04613fe0e195e37a28
Shabir Jan <iam.shabir@yahoo.com>
2017-08-01 01:55:44 +0500

    B-Tree Playground & Tests updated to Swift 4

--- B-Tree/Tests/Tests/BTreeNodeTests.swift
+++ B-Tree/Tests/Tests/BTreeNodeTests.swift
@@ -9,45 +9,50 @@
 import XCTest
 
 class BTreeNodeTests: XCTestCase {
-
-  let owner = BTree<Int, Int>(order: 2)!
-  var root: BTreeNode<Int, Int>!
-  var leftChild: BTreeNode<Int, Int>!
-  var rightChild: BTreeNode<Int, Int>!
-
-  override func setUp() {
-    super.setUp()
-
-    root = BTreeNode(owner: owner)
-    leftChild = BTreeNode(owner: owner)
-    rightChild = BTreeNode(owner: owner)
-
-    root.insert(1, for: 1)
-    root.children = [leftChild, rightChild]
-  }
-
-  func testIsLeafRoot() {
-    XCTAssertFalse(root.isLeaf)
-  }
-
-  func testIsLeafLeaf() {
-    XCTAssertTrue(leftChild.isLeaf)
-    XCTAssertTrue(rightChild.isLeaf)
-  }
-
-  func testOwner() {
-    XCTAssert(root.owner === owner)
-    XCTAssert(leftChild.owner === owner)
-    XCTAssert(rightChild.owner === owner)
-  }
-
-  func testNumberOfKeys() {
-    XCTAssertEqual(root.numberOfKeys, 1)
-    XCTAssertEqual(leftChild.numberOfKeys, 0)
-    XCTAssertEqual(rightChild.numberOfKeys, 0)
-  }
-
-  func testChildren() {
-    XCTAssertEqual(root.children!.count, 2)
-  }
+    
+    let owner = BTree<Int, Int>(order: 2)!
+    var root: BTreeNode<Int, Int>!
+    var leftChild: BTreeNode<Int, Int>!
+    var rightChild: BTreeNode<Int, Int>!
+    func testSwift4() {
+        // last checked with Xcode 9.0b4
+        #if swift(>=4.0)
+            print("Hello, Swift 4!")
+        #endif
+    }
+    override func setUp() {
+        super.setUp()
+        
+        root = BTreeNode(owner: owner)
+        leftChild = BTreeNode(owner: owner)
+        rightChild = BTreeNode(owner: owner)
+        
+        root.insert(1, for: 1)
+        root.children = [leftChild, rightChild]
+    }
+    
+    func testIsLeafRoot() {
+        XCTAssertFalse(root.isLeaf)
+    }
+    
+    func testIsLeafLeaf() {
+        XCTAssertTrue(leftChild.isLeaf)
+        XCTAssertTrue(rightChild.isLeaf)
+    }
+    
+    func testOwner() {
+        XCTAssert(root.owner === owner)
+        XCTAssert(leftChild.owner === owner)
+        XCTAssert(rightChild.owner === owner)
+    }
+    
+    func testNumberOfKeys() {
+        XCTAssertEqual(root.numberOfKeys, 1)
+        XCTAssertEqual(leftChild.numberOfKeys, 0)
+        XCTAssertEqual(rightChild.numberOfKeys, 0)
+    }
+    
+    func testChildren() {
+        XCTAssertEqual(root.children!.count, 2)
+    }
 }
