e7bce0ddcc03ac933f69f18b1115bb242cd2ddd3
Simon Whitaker <swhitaker@fb.com>
2017-09-05 19:34:47 +0100

    Add more toys to the playground

--- Multiset/Multiset.playground/Contents.swift
+++ Multiset/Multiset.playground/Contents.swift
@@ -25,7 +25,18 @@ set2.add("Foo")
 set2.isSubSet(of: set) // true
 set.isSubSet(of: set2) // false
 
+// Convenience constructor: pass a Collection of Hashables to the constructor
 var cacti = Multiset<Character>("cacti")
+cacti.allItems
 var tactical = Multiset<Character>("tactical")
 cacti.isSubSet(of: tactical) // true
 tactical.isSubSet(of: cacti) // false
+
+// Test ExpressibleByArrayLiteral protocol
+let set3: Multiset<String> = ["foo", "bar"]
+set3.count(for: "foo")
+
+// Test Equatable protocol
+let set4 = Multiset<String>(set3.allItems)
+set4 == set3 // true
+set4 == set // false
