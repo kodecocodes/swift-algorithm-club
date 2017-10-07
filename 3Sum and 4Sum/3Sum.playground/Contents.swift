ca6bd9f1c0c851e0561443c9b5bffdb926f38929
remlostime <remlostime@gmail.com>
2017-10-06 22:51:55 -0700

    address comments

--- 3Sum and 4Sum/3Sum.playground/Contents.swift
+++ 3Sum and 4Sum/3Sum.playground/Contents.swift
@@ -2,36 +2,36 @@ func ThreeSum(_ nums: [Int], targetSum: Int) -> [[Int]] {
     var a = nums.sorted()
     var ret: [[Int]] = []
     
-    for i in 0..<a.count {
-        if i != 0 && a[i] == a[i-1] {
+    for m in 0..<a.count {
+        if m != 0 && a[m] == a[m-1] {
             continue
         }
         
-        var j = i + 1
-        var k = nums.count - 1
+        var l = m + 1
+        var r = nums.count - 1
         
-        while j < k {
-            let sum = a[i] + a[j] + a[k]
+        while l < r {
+            let sum = a[m] + a[l] + a[r]
             
             if sum == targetSum {
-                ret.append([a[i], a[j], a[k]])
-                j += 1
+                ret.append([a[m], a[l], a[r]])
+                l += 1
             } else if sum < targetSum {
-                j += 1
+                l += 1
             } else {
-                k -= 1
+                r -= 1
             }
             
             if sum == targetSum {
-                while j < k {
+                while l < r {
                     var flag = false
-                    if j != 0 && a[j] == a[j-1] {
-                        j += 1
+                    if l != 0 && a[l] == a[l-1] {
+                        l += 1
                         flag = true
                     }
                     
-                    if (k != a.count - 1) && a[k] == a[k+1] {
-                        k -= 1
+                    if (r != a.count - 1) && a[r] == a[r+1] {
+                        r -= 1
                         flag = true
                     }
                     
@@ -51,3 +51,9 @@ ThreeSum([-1, 0, 1, 2, -1, -4], targetSum: 0)
 
 // Answer: [[-1, -1, 1]]
 ThreeSum([-1, 1, 1, -1], targetSum: 1)
+
+// Answer: [[-1, -1, 2], [-1, 0, 1]]
+ThreeSum([-1, -1, -1, -1, 2, 1, -4, 0], targetSum: 0)
+
+// Answer: [[-1, -1, 2]]
+ThreeSum([-1, -1, -1, -1, -1, -1, 2], targetSum: 0)
