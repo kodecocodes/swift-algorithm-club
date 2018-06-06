80aa4d420b16da3a2c724366608077aa218c7268
aatalyk <atalyk.akash@nu.edu.kz>
2018-06-06 05:12:47 -0700

    refactor implementation of an algorithm

--- Dynamic Programming/0-1 Knapsack/Knapsack.playground/Contents.swift
+++ Dynamic Programming/0-1 Knapsack/Knapsack.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-class KnapsackItem: NSObject {
+struct KnapsackItem {
+    
     var weight: Int
     var value: Int
     
@@ -10,40 +11,50 @@ class KnapsackItem: NSObject {
     }
 }
 
-var capacityOfBag = Int()
-var knapsackItems = [KnapsackItem]()
-
-func knapsack() -> Int {
+public final class Knapsack {
     
-    var tableOfValues = [[Int]]()
+    var capacityOfBag:Int
+    var knapsackItems: [KnapsackItem]
     
-    tableOfValues = Array(repeating: Array(repeating: 0, count: capacityOfBag+1),
-                          count: knapsackItems.count+1)
+    init(capacityOfBag: Int, knapsackItems: [KnapsackItem]) {
+        self.capacityOfBag = capacityOfBag
+        self.knapsackItems = knapsackItems
+    }
     
-    for itemIndex in 1...knapsackItems.count {
-        for totalWeight in 1...capacityOfBag {
-            
-            if totalWeight < knapsackItems[itemIndex-1].weight {
-                tableOfValues[itemIndex][totalWeight] = tableOfValues[itemIndex-1][totalWeight]
-            } else {
-                let remainingCapacity = totalWeight - knapsackItems[itemIndex-1].weight
-                tableOfValues[itemIndex][totalWeight] = max(knapsackItems[itemIndex-1].value + tableOfValues[itemIndex-1][remainingCapacity], tableOfValues[itemIndex-1][totalWeight])
+    func knapsack() -> Int {
+        
+        var tableOfValues = [[Int]]()
+        
+        tableOfValues = Array(repeating: Array(repeating: 0, count: capacityOfBag+1),
+                              count: knapsackItems.count+1)
+        
+        for itemIndex in 1...knapsackItems.count {
+            for totalWeight in 1...capacityOfBag {
+                
+                if totalWeight < knapsackItems[itemIndex-1].weight {
+                    tableOfValues[itemIndex][totalWeight] = tableOfValues[itemIndex-1][totalWeight]
+                } else {
+                    let remainingCapacity = totalWeight - knapsackItems[itemIndex-1].weight
+                    tableOfValues[itemIndex][totalWeight] = max(knapsackItems[itemIndex-1].value + tableOfValues[itemIndex-1][remainingCapacity], tableOfValues[itemIndex-1][totalWeight])
+                }
             }
         }
+        return tableOfValues[knapsackItems.count][capacityOfBag]
     }
-    return tableOfValues[knapsackItems.count][capacityOfBag]
 }
 
 func solution() {
     
-    capacityOfBag = 7
+    let capacityOfBag = 7
     
-    knapsackItems = [KnapsackItem(weight: 1, value: 1),
+    let knapsackItems = [KnapsackItem(weight: 1, value: 1),
                      KnapsackItem(weight: 3, value: 4),
                      KnapsackItem(weight: 4, value: 5),
                      KnapsackItem(weight: 5, value: 7)]
     
-    let result = knapsack()
+    let knapsack = Knapsack(capacityOfBag: capacityOfBag, knapsackItems: knapsackItems)
+    
+    let result = knapsack.knapsack()
     
     print(result)
 }
--- Dynamic Programming/0-1 Knapsack/Knapsack.swift
+++ Dynamic Programming/0-1 Knapsack/Knapsack.swift
@@ -1,4 +1,7 @@
-class KnapsackItem: NSObject {
+import Foundation
+
+struct KnapsackItem {
+    
     var weight: Int
     var value: Int
     
@@ -8,26 +11,34 @@ class KnapsackItem: NSObject {
     }
 }
 
-var capacityOfBag = Int()
-var knapsackItems = [KnapsackItem]()
-
-func knapsack() -> Int {
+public final class Knapsack {
     
-    var tableOfValues = [[Int]]()
+    var capacityOfBag:Int
+    var knapsackItems: [KnapsackItem]
     
-    tableOfValues = Array(repeating: Array(repeating: 0, count: capacityOfBag+1),
-                          count: knapsackItems.count+1)
+    init(capacityOfBag: Int, knapsackItems: [KnapsackItem]) {
+        self.capacityOfBag = capacityOfBag
+        self.knapsackItems = knapsackItems
+    }
     
-    for itemIndex in 1...knapsackItems.count {
-        for totalWeight in 1...capacityOfBag {
-            
-            if totalWeight < knapsackItems[itemIndex-1].weight {
-                tableOfValues[itemIndex][totalWeight] = tableOfValues[itemIndex-1][totalWeight]
-            } else {
-                let remainingCapacity = totalWeight - knapsackItems[itemIndex-1].weight
-                tableOfValues[itemIndex][totalWeight] = max(knapsackItems[itemIndex-1].value + tableOfValues[itemIndex-1][remainingCapacity], tableOfValues[itemIndex-1][totalWeight])
+    func knapsack() -> Int {
+        
+        var tableOfValues = [[Int]]()
+        
+        tableOfValues = Array(repeating: Array(repeating: 0, count: capacityOfBag+1),
+                              count: knapsackItems.count+1)
+        
+        for itemIndex in 1...knapsackItems.count {
+            for totalWeight in 1...capacityOfBag {
+                
+                if totalWeight < knapsackItems[itemIndex-1].weight {
+                    tableOfValues[itemIndex][totalWeight] = tableOfValues[itemIndex-1][totalWeight]
+                } else {
+                    let remainingCapacity = totalWeight - knapsackItems[itemIndex-1].weight
+                    tableOfValues[itemIndex][totalWeight] = max(knapsackItems[itemIndex-1].value + tableOfValues[itemIndex-1][remainingCapacity], tableOfValues[itemIndex-1][totalWeight])
+                }
             }
         }
+        return tableOfValues[knapsackItems.count][capacityOfBag]
     }
-    return tableOfValues[knapsackItems.count-1][capacityOfBag]
 }
