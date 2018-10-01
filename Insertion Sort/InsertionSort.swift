06e894bf80c8940fa20db172c291fa9be713350a
Julio Brazil <julio_bbl@hotmail.com>
2018-10-01 19:26:55 -0300

    added primitive documentation

--- Insertion Sort/InsertionSort.swift
+++ Insertion Sort/InsertionSort.swift
@@ -1,3 +1,9 @@
+/// Performs the Insertion sort algorithm to a given array
+///
+/// - Parameters:
+///   - array: the array of elements to be sorted
+///   - isOrderedBefore: returns true if the elements provided are in the corect order
+/// - Returns: a sorted array containing the same elements
 func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
   guard array.count > 1 else { return array }
 
@@ -14,6 +20,10 @@ func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
   return a
 }
 
+/// Performs the Insertion sort algorithm to a given array
+///
+/// - Parameter array: the array to be sorted, conatining elements that conform to the Comparable protocol
+/// - Returns: a sorted array containing the same elements
 func insertionSort<T: Comparable>(_ array: [T]) -> [T] {
     var a = array
     for x in 1..<a.count {
