d078c50bc281d92f0369fe3b6bac5508988e175a
Julio Brazil <julio_bbl@hotmail.com>
2018-10-01 16:02:04 -0300

    addition of function description

--- Selection Sort/SelectionSort.playground/Sources/SelectionSort.swift
+++ Selection Sort/SelectionSort.playground/Sources/SelectionSort.swift
@@ -1,3 +1,7 @@
+/// Performs the Selection sort algorithm on a array
+///
+/// - Parameter array: array of elements that conform to the Comparable protocol
+/// - Returns: an array in ascending order
 public func selectionSort<T: Comparable>(_ array: [T]) -> [T] {
   guard array.count > 1 else { return array }
 
@@ -20,6 +24,12 @@ public func selectionSort<T: Comparable>(_ array: [T]) -> [T] {
   return a
 }
 
+/// Performs the Selection sort algorithm on a array using the provided comparisson method
+///
+/// - Parameters:
+///   - array: array of elements that conform to the Comparable protocol
+///   - isLowerThan: returns true if the two provided elements are in the correct order
+/// - Returns: a sorted array
 public func selectionSort<T>(_ array: [T], _ isLowerThan: (T, T) -> Bool) -> [T] {
     guard array.count > 1 else { return array }
     
