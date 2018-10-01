d82954fc2c7533181c72cd800cd892a4bf7ab987
Julio Brazil <julio_bbl@hotmail.com>
2018-10-01 19:22:36 -0300

    added convenience method

--- Insertion Sort/InsertionSort.swift
+++ Insertion Sort/InsertionSort.swift
@@ -13,3 +13,17 @@ func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
   }
   return a
 }
+
+func insertionSort<T: Comparable>(_ array: [T]) -> [T] {
+    var a = array
+    for x in 1..<a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && temp < a[y - 1] {
+            a[y] = a[y - 1]
+            y -= 1
+        }
+        a[y] = temp
+    }
+    return a
+}
