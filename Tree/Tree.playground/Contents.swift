3f0bdc6ec0009bf08251458a04b11631d13cb7c7
danielchick <daniel.chick@hotmail.com>
2018-10-04 22:44:53 -0500

    [Swift 4.2] Update Tree

--- Tree/Tree.playground/Contents.swift
+++ Tree/Tree.playground/Contents.swift
@@ -1,8 +1,8 @@
 //: Playground - noun: a place where people can play
 
 // last checked with Xcode 9.0b4
-#if swift(>=4.0)
-print("Hello, Swift 4!")
+#if swift(>=4.2)
+print("Hello, Swift 4.2!")
 #endif
 
 let tree = TreeNode<String>(value: "beverages")
