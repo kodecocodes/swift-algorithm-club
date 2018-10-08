0e83ef2e5d287d737153a4c14d15ff8ed2cf7fca
Julio Brazil <julio_bbl@hotmail.com>
2018-10-08 19:30:25 -0300

    removed duplicate implementation

--- Bubble Sort/MyPlayground.playground/Contents.swift
+++ Bubble Sort/MyPlayground.playground/Contents.swift
@@ -1,22 +1,5 @@
 import Foundation
 
-public func BubbleSort<T> (_ elements: [T]) -> [T] where T: Comparable {
-    var array = elements
-    
-    for i in 0..<array.count {
-        for j in 1..<array.count-i {
-            print(array,"comparing [\(j-1)] with [\(j)]")
-            if array[j] < array[j-1] {
-                let tmp = array[j-1]
-                array[j-1] = array[j]
-                array[j] = tmp
-            }
-        }
-    }
-    
-    return array
-}
-
 var array = [4,2,1,3]
 
 print("before:",array)
