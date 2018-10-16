75783274fd291f6f9e6e801734c1eefa3b77eb38
Julio Brazil <julio_bbl@hotmail.com>
2018-10-16 11:35:51 -0300

    replaced repetitive implementation with call

--- Selection Sort/SelectionSort.playground/Sources/SelectionSort.swift
+++ Selection Sort/SelectionSort.playground/Sources/SelectionSort.swift
@@ -3,25 +3,7 @@
 /// - Parameter array: array of elements that conform to the Comparable protocol
 /// - Returns: an array in ascending order
 public func selectionSort<T: Comparable>(_ array: [T]) -> [T] {
-  guard array.count > 1 else { return array }
-
-  var a = array
-  for x in 0 ..< a.count - 1 {
-
-    // Find the lowest value in the rest of the array.
-    var lowest = x
-    for y in x + 1 ..< a.count {
-      if a[y] < a[lowest] {
-        lowest = y
-      }
-    }
-
-    // Swap the lowest value with the current array index.
-    if x != lowest {
-      a.swapAt(x, lowest)
-    }
-  }
-  return a
+  return insertionSort(array, <)
 }
 
 /// Performs the Selection sort algorithm on a array using the provided comparisson method
