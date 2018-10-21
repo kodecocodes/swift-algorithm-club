66d0b867e22378465d83aecce1c30609906538d5
Randy the Dev <37282011+RandyTheDev@users.noreply.github.com>
2018-10-22 03:05:09 +1100

    Updated Radix Sort to Swift 4.2

--- Radix Sort/RadixSort.playground/Contents.swift
+++ Radix Sort/RadixSort.playground/Contents.swift
@@ -1,21 +1,10 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
-
-// last checked with Xcode 9.0b4
-#if swift(>=4.0)
-print("Hello, Swift 4!")
-#endif
-
 // Test Radix Sort with small array of ten values
 var array: [Int] = [19, 4242, 2, 9, 912, 101, 55, 67, 89, 32]
 radixSort(&array)
 
 // Test Radix Sort with large array of 1000 random values
-var bigArray = [Int](repeating: 0, count: 1000)
-var i = 0
-while i < 1000 {
-  bigArray[i] = Int(arc4random_uniform(1000) + 1)
-  i += 1
-}
+var bigArray = (0..<1000).map { _ in Int.random(in: 1...1000) }
+bigArray
 radixSort(&bigArray)
