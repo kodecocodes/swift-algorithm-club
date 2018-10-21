36ae366ea4faeb4bf96380df3d58ca4da34525c6
Randy the Dev <37282011+RandyTheDev@users.noreply.github.com>
2018-10-22 04:01:21 +1100

    Updated Radix Sort Tests to Swift 4.2

--- Radix Sort/Tests/RadixSortTests.swift
+++ Radix Sort/Tests/RadixSortTests.swift
@@ -9,13 +9,6 @@
 import XCTest
 
 class RadixSortTests: XCTestCase {
-  func testSwift4() {
-    // last checked with Xcode 9.0b4
-    #if swift(>=4.0)
-      print("Hello, Swift 4!")
-    #endif
-  }
-  
   func testCombSort() {
     let expectedSequence: [Int] = [2, 9, 19, 32, 55, 67, 89, 101, 912, 4242]
     var sequence = [19, 4242, 2, 9, 912, 101, 55, 67, 89, 32]
