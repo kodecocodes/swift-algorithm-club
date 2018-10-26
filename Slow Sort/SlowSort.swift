b47eb3e80afffc20c5e540e33fec66ae7a65bec0
laurentaylormarshall <44420971+laurentaylormarshall@users.noreply.github.com>
2018-10-26 18:56:13 -0500

    Update SlowSort.swift

--- Slow Sort/SlowSort.swift
+++ Slow Sort/SlowSort.swift
@@ -5,10 +5,8 @@
 //  Created by Pope Lukas Schramm (Dabendorf Orthodox Religion) on 16-07-16.
 //
 //
-
-public func slowsort(_ i: Int, _ j: Int, _ numberList: inout [Int]) {
-  guard i<j else { return }
-    
+func slowSort(_ i: Int, _ j: Int, _ numberList: inout [Int]) {
+  guard if i < j else { return }
   let m = (i+j)/2
   slowsort(i, m, &numberList)
   slowsort(m+1, j, &numberList)
