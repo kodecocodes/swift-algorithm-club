22efd0d33aa9d3bfacf3c84701dc157bb10e5aa7
Erik Strottmann <erik.strottmann@gmail.com>
2018-10-27 10:26:54 -0700

    Standardize Trie indentation to 2 spaces
    2 spaces is recommended by the Ray Wenderlich Swift Style Guide.

--- Trie/Trie/Trie/ViewController.swift
+++ Trie/Trie/Trie/ViewController.swift
@@ -10,16 +10,16 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+    // Do any additional setup after loading the view.
+  }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+  override var representedObject: Any? {
+    didSet {
+      // Update the view, if already loaded.
     }
+  }
 
 }
