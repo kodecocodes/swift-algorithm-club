f1363dd983ccacf1e48d9d393022f3ce68ba3c39
Francesco Scalise <f.scalise@eco-mind.eu>
2018-12-04 19:31:26 +0100

    [Swift 4.2] Count occurences

--- Count Occurrences/CountOccurrences.playground/Contents.swift
+++ Count Occurrences/CountOccurrences.playground/Contents.swift
@@ -1,7 +1,3 @@
-// last checked with Xcode 9.0b4
-#if swift(>=4.0)
-  print("Hello, Swift 4!")
-#endif
 
 func countOccurrencesOfKey(_ key: Int, inArray a: [Int]) -> Int {
   func leftBoundary() -> Int {
