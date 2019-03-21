91b7bb1d1d16f4cbe64399de156b2045ebf6d5be
Kelvin Lau <kelvinlauKL@users.noreply.github.com>
2019-03-21 10:17:06 -0400

    Update SlowSort.swift

--- Slow Sort/SlowSort.swift
+++ Slow Sort/SlowSort.swift
@@ -7,14 +7,14 @@
 //
 
 func slowSort(_ i: Int, _ j: Int, _ numberList: inout [Int]) {
-    guard if i < j else { return }
-    let m = (i+j)/2
-    slowSort(i, m, &numberList)
-    slowSort(m+1, j, &numberList)
-    if numberList[j] < numberList[m] {
-        let temp = numberList[j]
-        numberList[j] = numberList[m]
-        numberList[m] = temp
-    }
-    slowSort(i, j-1, &numberList)
+  guard if i < j else { return }
+  let m = (i+j)/2
+  slowSort(i, m, &numberList)
+  slowSort(m+1, j, &numberList)
+  if numberList[j] < numberList[m] {
+      let temp = numberList[j]
+      numberList[j] = numberList[m]
+      numberList[m] = temp
+  }
+  slowSort(i, j-1, &numberList)
 }
