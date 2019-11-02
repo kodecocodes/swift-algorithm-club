df014ca7013f4e8ac242830a6f372c9b5757034a
Kelvin Lau <kelvin.kl.lau@gmail.com>
2019-11-01 23:09:15 -0400

    Adds brute force string search

---
+++ SwiftAlgorithmClub/BruteForceStringSearch.playground/Contents.swift
@@ -0,0 +1,29 @@
+extension String {
+  func indexOf(_ pattern: String) -> String.Index? {
+
+    for i in self.characters.indices {
+      var j = i
+      var found = true
+      for p in pattern.characters.indices {
+        if j == self.characters.endIndex || self[j] != pattern[p] {
+          found = false
+          break
+        } else {
+            j = self.characters.index(after: j)
+        }
+      }
+      if found {
+        return i
+      }
+    }
+    return nil
+  }
+}
+
+// A few simple tests
+
+let s = "Hello, World"
+s.indexOf("World")  // 7
+
+let animals = "🐶🐔🐷🐮🐱"
+animals.indexOf("🐮")  // 6
