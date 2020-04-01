20f224436983078568ea35a0c8eabc2f1819b8ed
Nikita <47992492+Kn1kt@users.noreply.github.com>
2020-04-02 01:46:46 +0300

    four space indentation

--- Rabin-Karp/rabin-karp.swift
+++ Rabin-Karp/rabin-karp.swift
@@ -21,54 +21,54 @@
 // SOFTWARE.
 
 public func hash(_ string: String) -> Int {
-  return string.reduce(0) { (result: Int, character: Character) in
-    result &+ character.hashValue
-  }
+    return string.reduce(0) { (result: Int, character: Character) in
+        result &+ character.hashValue
+    }
 }
 
 public func nextHash(prevHash: Int, dropped: Character, added: Character) -> Int {
-  return prevHash &- dropped.hashValue &+ added.hashValue
+    return prevHash &- dropped.hashValue &+ added.hashValue
 }
 
 public func search(text: String, pattern: String) -> [String.Index] {
-  if text.count < pattern.count { return [] }
-  
-  var indices = [String.Index]()
-  
-  let patternHash = hash(pattern)
-  let patternLength = pattern.count - 1
-  let offset = text.index(text.startIndex, offsetBy: patternLength)
-  let firstText = String(text[...offset])
-  let firstHash = hash(firstText)
-  
-  if patternHash == firstHash {
-    if firstText == pattern {
-      indices.append(text.startIndex)
-    }
-  }
-  
-  let start = text.index(after: text.startIndex)
-  let end = text.index(text.endIndex, offsetBy: -patternLength)
-  var prevHash = firstHash
-  var i = start
-
-  while i != end {
-    let terminator = text.index(i, offsetBy: patternLength)
-    let window = text[i...terminator]
-    let prev = text.index(before: i)
-    let windowHash = nextHash(prevHash: prevHash, dropped: text[prev], added: text[terminator])
+    if text.count < pattern.count { return [] }
     
-    if windowHash == patternHash,
-      pattern == window {
-      indices.append(i)
+    var indices = [String.Index]()
+    
+    let patternHash = hash(pattern)
+    let patternLength = pattern.count - 1
+    let offset = text.index(text.startIndex, offsetBy: patternLength)
+    let firstText = String(text[...offset])
+    let firstHash = hash(firstText)
+    
+    if patternHash == firstHash {
+        if firstText == pattern {
+            indices.append(text.startIndex)
+        }
     }
     
-    prevHash = windowHash
+    let start = text.index(after: text.startIndex)
+    let end = text.index(text.endIndex, offsetBy: -patternLength)
+    var prevHash = firstHash
+    var i = start
     
-    i = text.index(after: i)
-  }
-  
-  return indices
+    while i != end {
+        let terminator = text.index(i, offsetBy: patternLength)
+        let window = text[i...terminator]
+        let prev = text.index(before: i)
+        let windowHash = nextHash(prevHash: prevHash, dropped: text[prev], added: text[terminator])
+        
+        if windowHash == patternHash,
+            pattern == window {
+            indices.append(i)
+        }
+        
+        prevHash = windowHash
+        
+        i = text.index(after: i)
+    }
+    
+    return indices
 }
 
 // TESTS
