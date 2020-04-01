52fe0199a63b77c0e87208748a100dd1a92c7faa
Nikita <47992492+Kn1kt@users.noreply.github.com>
2020-04-01 17:27:53 +0300

    Little simplified code and update to Swift 5.1

--- Knuth-Morris-Pratt/KnuthMorrisPratt.swift
+++ Knuth-Morris-Pratt/KnuthMorrisPratt.swift
@@ -8,58 +8,68 @@
 
 import Foundation
 
+func indents(_ pattern: String) -> [Int]? {
+  guard pattern.count > 0 else { return nil }
+  let start = pattern.startIndex
+  let length = pattern.count
+  
+  var indents = Array(repeating: 0, count: length)
+  var i = 1
+  var j = 0
+  
+  while i < length {
+    let pi = pattern.index(start, offsetBy: i)
+    let pj = pattern.index(start, offsetBy: j)
+    
+    if pattern[pi] == pattern[pj] {
+      indents[i] = j + 1
+      i += 1
+      j += 1
+      
+    } else if j == 0 {
+      indents[i] = 0
+      i += 1
+      
+    } else {
+      j = indents[j - 1]
+    }
+  }
+  
+  return indents
+}
+
 extension String {
   
-  func indexesOf(ptnr: String) -> [Int]? {
+  func indices(of pattern: String) -> [Index] {
+    guard let indents = indents(pattern) else { return [] }
     
-    let text = Array(self.characters)
-    let pattern = Array(ptnr.characters)
+    var indices = [Index]()
     
-    let textLength: Int = text.count
-    let patternLength: Int = pattern.count
+    let pLength = pattern.count
+    let sLength = count
+    var k = 0
+    var l = 0
     
-    guard patternLength > 0 else {
-      return nil
-    }
-    
-    var suffixPrefix: [Int] = [Int](repeating: 0, count: patternLength)
-    var textIndex: Int = 0
-    var patternIndex: Int = 0
-    var indexes: [Int] = [Int]()
-    
-    /* Pre-processing stage: computing the table for the shifts (through Z-Algorithm) */
-    let zeta = ZetaAlgorithm(ptnr: ptnr)
-    
-    for patternIndex in (1 ..< patternLength).reversed() {
-      textIndex = patternIndex + zeta![patternIndex] - 1
-      suffixPrefix[textIndex] = zeta![patternIndex]
-    }
-    
-    /* Search stage: scanning the text for pattern matching */
-    textIndex = 0
-    patternIndex = 0
-    
-    while textIndex + (patternLength - patternIndex - 1) < textLength {
+    while k < sLength {
+      let sk = index(startIndex, offsetBy: k)
+      let pl = pattern.index(pattern.startIndex, offsetBy: l)
       
-      while patternIndex < patternLength && text[textIndex] == pattern[patternIndex] {
-        textIndex = textIndex + 1
-        patternIndex = patternIndex + 1
-      }
-      
-      if patternIndex == patternLength {
-        indexes.append(textIndex - patternIndex)
-      }
-      
-      if patternIndex == 0 {
-        textIndex = textIndex + 1
+      if self[sk] == pattern[pl] {
+        k += 1
+        l += 1
+        if l == pLength {
+          let index = self.index(sk, offsetBy: -(pLength - 1))
+          indices.append(index)
+          l = 0
+        }
+        
+      } else if l == 0 {
+        k += 1
       } else {
-        patternIndex = suffixPrefix[patternIndex - 1]
+        l = indents[l - 1]
       }
     }
     
-    guard !indexes.isEmpty else {
-      return nil
-    }
-    return indexes
+    return indices
   }
 }
