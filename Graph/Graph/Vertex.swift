e515919e305371cbff38319e00d21c90b35188e4
Richard Ash <r.ash8347@gmail.com>
2020-08-29 14:24:59 -0700

    Update Vertex.swift

--- Graph/Graph/Vertex.swift
+++ Graph/Graph/Vertex.swift
@@ -24,16 +24,10 @@ extension Vertex: CustomStringConvertible {
 
 extension Vertex: Hashable {
 
-
-
-    
-    public func hasher(into hasher: inout Hasher){
-        
-        hasher.combine(data)
-        hasher.combine(index)
-    }
-
-
+  public func hasher(into hasher: inout Hasher) {
+    hasher.combine(data)
+    hasher.combine(index)
+  }
 
 }
 
