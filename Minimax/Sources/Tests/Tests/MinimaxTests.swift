6fb1f520ffcb8b4269d2107c902c0bf78f1bf4ff
Michał Nowak <michal.nowak061@gmail.com>
2021-06-21 22:31:38 +0200

    Initial commit

---
+++ Minimax/Sources/Tests/Tests/MinimaxTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+
+class MinimaxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
