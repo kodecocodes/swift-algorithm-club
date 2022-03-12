b41b80d8a05ec15b1dfd5e45a0c9de299f77a1de
prokhorovxo <prokhorovxo@yahoo.com>
2022-03-12 12:06:56 +0200

    Update BinarySearch.swift
    Replace if with guard
    https://github.com/raywenderlich/swift-style-guide#golden-path

--- Binary Search/BinarySearch.swift
+++ Binary Search/BinarySearch.swift
@@ -14,17 +14,17 @@ import Foundation
 // The recursive version of binary search.
 
 public func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
-    if range.lowerBound >= range.upperBound {
+    guard range.lowerBound < range.upperBound else {
         return nil
+    }
+    
+    let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+    if a[midIndex] > key {
+        return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
+    } else if a[midIndex] < key {
+        return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
     } else {
-        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-        if a[midIndex] > key {
-            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
-        } else if a[midIndex] < key {
-            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
-        } else {
-            return midIndex
-        }
+        return midIndex
     }
 }
 
